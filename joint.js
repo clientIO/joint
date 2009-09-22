0b216345e1d0e0ee119c81651d9b784d925f261b
DavidDurman <daviddurman@seznam.cz>
2009-09-22 22:11:21 +0200

    cross-browser event handling

--- joint.js
+++ joint.js
@@ -1,6 +1,6 @@
 var NDEBUG = false;
 /****************************************************
- * Joint 0.1.2 - JavaScript Connection Library
+ * Joint 0.1.2 - JavaScript library for connecting vector objects
  *
  * Copyright (c) 2009 David Durman
  *
@@ -314,9 +314,80 @@ Joint.mouseUp = function(e){
     Joint.draggedCap = null;	// cap is no longer dragged
 };
 
-// TODO: platform neutral event listeners
-document.addEventListener("mousemove", Joint.mouseMove, false);
-document.addEventListener("mouseup", Joint.mouseUp, false);
+
+/**
+ * Cross-browser event handling.
+ * From Dean Edwards' addEvent library.
+ */
+function addEvent(element, type, handler) {
+    if (element.addEventListener) {
+	element.addEventListener(type, handler, false);
+    } else {
+	// assign each event handler a unique ID
+	if (!handler.$$guid) handler.$$guid = addEvent.guid++;
+	// create a hash table of event types for the element
+	if (!element.events) element.events = {};
+	// create a hash table of event handlers for each element/event pair
+	var handlers = element.events[type];
+	if (!handlers) {
+	    handlers = element.events[type] = {};
+	    // store the existing event handler (if there is one)
+	    if (element["on" + type]) {
+		handlers[0] = element["on" + type];
+	    }
+	}
+	// store the event handler in the hash table
+	handlers[handler.$$guid] = handler;
+	// assign a global event handler to do all the work
+	element["on" + type] = handleEvent;
+    }
+};
+// a counter used to create unique IDs
+addEvent.guid = 1;
+
+function removeEvent(element, type, handler) {
+    if (element.removeEventListener) {
+	element.removeEventListener(type, handler, false);
+    } else {
+	// delete the event handler from the hash table
+	if (element.events && element.events[type]) {
+	    delete element.events[type][handler.$$guid];
+	}
+    }
+};
+
+function handleEvent(event) {
+    var returnValue = true;
+    // grab the event object (IE uses a global event object)
+    event = event || fixEvent(((this.ownerDocument || this.document || this).parentWindow || window).event);
+    // get a reference to the hash table of event handlers
+    var handlers = this.events[event.type];
+    // execute each event handler
+    for (var i in handlers) {
+	this.$$handleEvent = handlers[i];
+	if (this.$$handleEvent(event) === false) {
+	    returnValue = false;
+	}
+    }
+    return returnValue;
+};
+
+function fixEvent(event) {
+    // add W3C standard event methods
+    event.preventDefault = fixEvent.preventDefault;
+    event.stopPropagation = fixEvent.stopPropagation;
+    return event;
+};
+fixEvent.preventDefault = function() {
+    this.returnValue = false;
+};
+fixEvent.stopPropagation = function() {
+    this.cancelBubble = true;
+};
+
+
+addEvent(document, "mousemove", Joint.mouseMove);
+addEvent(document, "mouseup", Joint.mouseUp);
 
 
 /**************************************************
