268d7ade2366f18f383787630a66b67f24ddde48
David Durman <org@client.io>
2013-09-10 16:41:51 +0200

    fix paper>>resetCells() so that links are added always after elements and then resorted according to z

--- test/joint.dia/paper.js
+++ test/joint.dia/paper.js
@@ -22,7 +22,7 @@ module('paper', {
     }
 });
 
-test('resetCells()', function() {
+test('graph.resetCells()', function() {
 
     var r1 = new joint.shapes.basic.Rect;
     var r2 = new joint.shapes.basic.Rect;
@@ -33,6 +33,29 @@ test('resetCells()', function() {
 
     equal(this.graph.get('cells').length, 2, 'previous cells were removed from the graph after calling graph.resetCells()');
     equal(this.paper.$('.element').length, 2, 'previous cells were removed from the paper after calling graph.resetCells()');
-    
+});
+
+test('graph.fromJSON()', function() {
+
+    var json = {
+        "cells":[
+            {"type":"basic.Circle","size":{"width":100,"height":60},"position":{"x":110,"y":480},"id":"bbb9e641-9756-4f42-997a-f4818b89f374","embeds":"","z":0},
+            {"type":"link","source":{"id":"bbb9e641-9756-4f42-997a-f4818b89f374"},"target":{"id":"cbd1109e-4d34-4023-91b0-f31bce1318e6"},"id":"b4289c08-07ea-49d2-8dde-e67eb2f2a06a","z":1},
+            {"type":"basic.Rect","position":{"x":420,"y":410},"size":{"width":100,"height":60},"id":"cbd1109e-4d34-4023-91b0-f31bce1318e6","embeds":"","z":2}
+        ]
+    };
+
+    this.graph.fromJSON(json);
+
+    equal(this.graph.get('cells').length, 3, 'all the cells were reconstructed from JSON');
+
+    // Check that the link is before the last cell in the DOM. This check is there because
+    // paper might have resorted the cells so that links are always AFTER elements.
+    var linkView = this.paper.findViewByModel('b4289c08-07ea-49d2-8dde-e67eb2f2a06a');
+    var rectView = this.paper.findViewByModel('cbd1109e-4d34-4023-91b0-f31bce1318e6');
+    var circleView = this.paper.findViewByModel('bbb9e641-9756-4f42-997a-f4818b89f374');
+
+    ok(rectView.el.previousSibling === linkView.el, 'link view is before rect element in the DOM');
+    ok(linkView.el.previousSibling === circleView.el, 'link view is after circle element in the DOM');
 });
 
