4de06254e101b2114fb7604932c9ed5d2a305b98
areel <aidanreel@gmail.com>
2013-09-17 18:47:28 +0100

    Default smooth line uses bezier control points: Handle case were source point X is greater than target Point X

--- src/joint.dia.link.js
+++ src/joint.dia.link.js
@@ -558,11 +558,14 @@ joint.dia.LinkView = joint.dia.CellView.extend({
             if (vertices && vertices.length) {
                 d = g.bezier.curveThroughPoints([sourcePoint].concat(vertices || []).concat([targetPoint]));
             } else {
-                // if we have no vertices use a default cubic bezier curve
-                // the control points are defined with X as mid way between the start and end points, Y being equal to start/end Y values.
-                // this gives a smooth S shape.
-                var controlPointX = targetPoint.x - (Math.abs(targetPoint.x - sourcePoint.x) / 2);
-                d = ['M', sourcePoint.x, sourcePoint.y, 'C', controlPointX, sourcePoint.y, controlPointX, targetPoint.y, targetPoint.x, targetPoint.y];
+                // if we have no vertices use a default cubic bezier curve, cubic bezier requires two control points.
+                // the two control points are both defined with X as mid way between the source and target points.
+                // sourceControlPoint Y is equal to sourcePoint Y and targetControlPointY being equal to targetPointY.
+                // handle situation were sourcePointX is greater or less then targetPointX.
+                var controlPointX = (sourcePoint.x < targetPoint.x) 
+                    ? targetPoint.x - ((targetPoint.x - sourcePoint.x) / 2)
+                    : sourcePoint.x - ((sourcePoint.x - targetPoint.x) / 2);
+                    d = ['M', sourcePoint.x, sourcePoint.y, 'C', controlPointX, sourcePoint.y, controlPointX, targetPoint.y, targetPoint.x, targetPoint.y];
             }
             
         } else {
