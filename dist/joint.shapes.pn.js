72437c5a64787032ff608183be0f1bb25db9dda4
Roman Bruckner <bruckner.roman@gmail.com>
2014-03-14 19:47:15 +0000

    make dist

--- dist/joint.shapes.pn.js
+++ dist/joint.shapes.pn.js
@@ -1,4 +1,4 @@
-/*! JointJS v0.8.1 - JavaScript diagramming library  2014-02-24 
+/*! JointJS v0.8.0 - JavaScript diagramming library  2014-03-14 
 
 
 This Source Code Form is subject to the terms of the Mozilla Public
