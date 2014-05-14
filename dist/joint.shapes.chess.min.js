3edfc197690c2852a63a027922172a2f4456a873
David Durman <org@client.io>
2014-05-14 08:59:10 +0200

    make dist

--- dist/joint.shapes.chess.min.js
+++ dist/joint.shapes.chess.min.js
@@ -1,4 +1,4 @@
-/*! JointJS v0.8.1 - JavaScript diagramming library  2014-05-13 
+/*! JointJS v0.9.0 - JavaScript diagramming library  2014-05-14 
 
 
 This Source Code Form is subject to the terms of the Mozilla Public
