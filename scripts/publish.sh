ae078c8815c1d1a6c14862fa6c58e8e3df7f1fdc
Charles Hill <chill@degreesofzero.com>
2016-07-04 16:38:05 +0200

    Added publish script, which does the following:
    * Runs tests (aborts publishing if fails)
    * Builds dist files
    * Commits dist files
    * Creates git tag for version in package.json file

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,46 @@
 #!/bin/bash
 
-# Rebuild dist files, run all tests, and finally publish to npm only if everything is OK.
-grunt dist && grunt test && npm publish;
+packageJsonFile="$( cd "$( dirname "$0" )" && pwd )/../package.json";
+re='version": ?"([^"]+)",?';
+versionLine="$(grep -Po 'version": ?"([^"]+)",?' $packageJsonFile)";
+if [[ $versionLine =~ $re ]]; then
+	version="${BASH_REMATCH[1]}";
+fi;
+
+if [ -z "$version" ]; then
+	echo "Failed to extract package version number.";
+	exit 1;
+fi;
+
+# Check if this version has already been published.
+if git rev-parse -q --verify "refs/tags/v$version" > /dev/null; then
+    echo "JointJS v$version has already been published.";
+    exit 1;
+fi;
+
+nl=$'\n';
+read -p "You are about to publish JointJS v$version${nl}Do you want to continue? (y/n) " ANSWER;
+
+if [ "$ANSWER" = "y" ]; then
+	# Continue with publishing.
+
+	# Rebuild dist files, run all tests.
+	grunt dist && grunt test;
+
+	if [ $? -eq 0 ]; then
+		echo "${nl}All tests passed. Continuing with publishing.";
+
+		# Commit changes to dist directory.
+		git add dist && git commit dist -m "Release v$version";
+
+		# Create git tag.
+		git tag -a "v$version" -m "Release v$version" && git push upstream && git push upstream v$version;
+	else
+		echo "${nl}Aborted due to errors.";
+		exit 1;
+	fi;
+else
+	echo "${nl}Canceled.";
+fi;
+
+exit 0;
