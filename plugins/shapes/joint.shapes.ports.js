f68f89b7ba91be7e081fd84055bcb8738b197f77
pskala <pskala@seznam.cz>
2016-07-19 10:13:13 +0200

    fix issues

--- plugins/shapes/joint.shapes.ports.js
+++ plugins/shapes/joint.shapes.ports.js
@@ -46,42 +46,30 @@ joint.shapes.ports.Model = joint.shapes.basic.Generic.extend({
     }, joint.shapes.basic.Generic.prototype.defaults),
 
     initialize: function () {
-        var that = this;
-        this.listenTo(this, 'change:inPorts', function (cell, changed, opt) {
-            that.changePorts(cell, opt, 'inPorts');
-        });
-        this.listenTo(this, 'change:outPorts', function (cell, changed, opt) {
-            that.changePorts(cell, opt, 'outPorts');
-        });
+        joint.shapes.basic.Generic.prototype.initialize.apply(this, arguments);
+        this.on('change:inPorts change:outPorts', this.updatePortItems, this);
     },
 
-    changePorts: function (model, opt, type) {
-        var ports = model.get(type);
+    updatePortItems: function (model, changed, opt) {
         var group = 'out';
         var filter = 'in';
+        if (this.changed.inPorts) {
+            group = 'in';
+            filter = 'out';
+        }
         var groupFilter = function(array, groupName) {
             return _.filter(array, function(element) {
               return (element.group === groupName );
             });
         };
 
-        if (type === 'inPorts') {
-            group = 'in';
-            filter = 'out';
-        }
-
         var portsItems = groupFilter(model.prop('ports/items'), filter);
 
-        model.prop('ports/items', _.map(ports, function (p) {
+        model.prop('ports/items', _.map(changed, function (p) {
             return {id: p, group: group, attrs: {text: {text: p}}};
         }), _.extend({rewrite: true}, opt));
 
         model.prop('ports/items', model.prop('ports/items').concat(portsItems));
-    },
-    portData: {
-            getPorts: function() {
-                return [];
-            }
     }
 });
 
@@ -91,14 +79,12 @@ joint.shapes.ports.Atomic = joint.shapes.ports.Model.extend({
         type: 'ports.Atomic',
         position: { x: 250, y: 250 },
         size: { width: 180, height: 80 },
-        markup: '<rect width="180" height="80" stroke="red"/>',
+        markup: '<rect/>',
         attrs: {
-            '.body': {
-                fill: 'salmon'
-            },
-            '.label': {
+            text: {
                 text: 'Atomic'
-            }
+            },
+            rect: { stroke: '#31d0c6', 'stroke-width': 2, fill: '#ffffff' }
         }
     }, joint.shapes.ports.Model.prototype.defaults)
 });
