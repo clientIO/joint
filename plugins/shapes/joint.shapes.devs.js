53afe69f3364ad0864d40060d04702cd7266fd38
pskala <pskala@seznam.cz>
2016-07-22 09:48:16 +0200

    renamed demo, add highlighting, reformat source code

--- plugins/shapes/joint.shapes.devs.js
+++ plugins/shapes/joint.shapes.devs.js
@@ -6,88 +6,100 @@ joint.shapes.devs = {};
 joint.shapes.devs.Model = joint.shapes.basic.Generic.extend({
 
     markup: '<g class="rotatable"><rect class="body"/><text class="label"/></g>',
-
     defaults: joint.util.deepSupplement({
-    type: 'ports.Model',
-    size: { width: 1, height: 1 },
-    attrs: {
-        '.label': {
-            'pointer-events': 'none',
-            text: 'Model',
-            'ref-x': .5,
-            'ref-y': 10,
-            ref: '.body',
-            'font-size': 18,
-            'text-anchor': 'middle',
-            fill: '#000'
-        },
-        '.body': {
-            'ref-width': '100%',
-            'ref-height': '100%'
-        }
-    },
-    ports: {
-        groups: {
-            'in': {
-                position: {
-                    name: 'left',
-                    args: {}
-                },
-                markup: '<circle class="input-port"/>',
-                attrs: {
-                    text: {
-                        fill: 'black'
-                    },
-                    circle: {
-                        fill: 'PaleGreen',
-                        r: '10',
-                        magnet: false
-                    }
-                }
+
+        type: 'ports.Model',
+        size: { width: 1, height: 1 },
+        attrs: {
+            '.label': {
+                'pointer-events': 'none',
+                text: 'Model',
+                'ref-x': .5,
+                'ref-y': 10,
+                ref: '.body',
+                'font-size': 18,
+                'text-anchor': 'middle',
+                fill: '#000'
             },
-            'out': {
-                position: {
-                    name: 'right',
-                    args: {}
-                },
-                markup: '<circle class="output-port"/>',
-                attrs: {
-                    text: {
-                        fill: 'blue'
+            '.body': {
+                'ref-width': '100%',
+                'ref-height': '100%'
+            }
+        },
+        ports: {
+            groups: {
+                'in': {
+                    position: {
+                        name: 'left',
+                        args: {}
                     },
-                    circle: {
-                        fill: 'Tomato',
-                        r: '10',
-                        magnet: true
+                    markup: '<circle class="input-port"/>',
+                    attrs: {
+                        text: {
+                            fill: 'black'
+                        },
+                        circle: {
+                            fill: 'PaleGreen',
+                            r: '10'
+                        }
+                    },
+                    label: {
+                        position: {
+                            name: 'left',
+                            args: {
+                                y: 10
+                            }
+                        }
                     }
                 },
-                label: {
+                'out': {
                     position: {
-                        name : 'right'
+                        name: 'right',
+                        args: {}
+                    },
+                    markup: '<circle class="output-port"/>',
+                    attrs: {
+                        text: {
+                            fill: 'blue'
+                        },
+                        circle: {
+                            fill: 'Tomato',
+                            r: '10',
+                            magnet: true
+                        }
+                    },
+                    label: {
+                        position: {
+                            name: 'right',
+                            args: {
+                                y: 10
+                            }
+                        }
                     }
                 }
             }
         }
-    }}, joint.shapes.basic.Generic.prototype.defaults),
-
+    }, joint.shapes.basic.Generic.prototype.defaults),
     initialize: function() {
+
         joint.shapes.basic.Generic.prototype.initialize.apply(this, arguments);
+
         this.on('change:inPorts change:outPorts', this.updatePortItems, this);
         this.updatePortItems();
     },
-
     updatePortItems: function(model, changed, opt) {
+
         var inPortItems = this.createPortItems('in', this.get('inPorts'));
         var outPortItems = this.createPortItems('out', this.get('outPorts'));
 
         this.prop('ports/items', inPortItems.concat(outPortItems), _.extend({ rewrite: true }, opt));
     },
-
     createPortItem: function(group, port) {
+
         return { id: port, group: group, attrs: { text: { text: port }}};
     },
-
     createPortItems: function(group, ports) {
+
         return _.map(ports, _.bind(this.createPortItem, this, group));
     }
 });
@@ -95,6 +107,7 @@ joint.shapes.devs.Model = joint.shapes.basic.Generic.extend({
 joint.shapes.devs.Atomic = joint.shapes.devs.Model.extend({
 
     defaults: joint.util.deepSupplement({
+
         type: 'devs.Atomic',
         size: { width: 80, height: 80 },
         attrs: {
@@ -108,6 +121,7 @@ joint.shapes.devs.Atomic = joint.shapes.devs.Model.extend({
 joint.shapes.devs.Coupled = joint.shapes.devs.Model.extend({
 
     defaults: joint.util.deepSupplement({
+
         type: 'devs.Coupled',
         size: { width: 200, height: 300 },
         attrs: {
