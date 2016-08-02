ce056acbb45009e75a15be9ad43ff4035e7b0bd5
Roman Bruckner <bruckner.roman@gmail.com>
2016-08-02 11:40:07 +0200

    Update joint.highlighters.opacity.js

--- plugins/highlighters/joint.highlighters.opacity.js
+++ plugins/highlighters/joint.highlighters.opacity.js
@@ -5,7 +5,7 @@ joint.highlighters.opacity = {
      * @param {Element} magnetEl
      * @param {object=} opt
      */
-    highlight: function(cellView, magnetEl, opt) {
+    highlight: function(cellView, magnetEl) {
 
         V(magnetEl).addClass(joint.util.addClassNamePrefix('highlight-opacity'));
     },
@@ -15,7 +15,7 @@ joint.highlighters.opacity = {
      * @param {Element} magnetEl
      * @param {object=} opt
      */
-    unhighlight: function(cellView, magnetEl, opt) {
+    unhighlight: function(cellView, magnetEl) {
 
         V(magnetEl).removeClass(joint.util.addClassNamePrefix('highlight-opacity'));
     }
