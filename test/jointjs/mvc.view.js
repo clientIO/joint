c628b0e0e1cd552448f197c24da1a4aec5522468
Charles Hill <chill117@users.noreply.github.com>
2017-03-22 08:50:12 +0100

    Clean-up of mvc.view and tests (#548)

--- test/jointjs/mvc.view.js
+++ test/jointjs/mvc.view.js
@@ -1,179 +1,173 @@
-module('joint.mvc.View', {
+'use strict';
 
-    beforeEach: function() {
+QUnit.module('joint.mvc.View', function(hooks) {
 
+    var resetViews = function() {
         _.invoke(joint.mvc.views, 'remove');
         joint.mvc.views = {};
-    },
-
-    afterEach: function() {
-
-        _.invoke(joint.mvc.views, 'remove');
-        joint.mvc.views = {};
-    }
-});
-
-test('constructor', function(assert) {
-
-    assert.ok(typeof joint.mvc.View === 'function', 'should be a function');
-
-    var setThemeCalled = false;
-    var className = 'class-name1 class-name2';
-
-    var SomeView = joint.mvc.View.extend({
-        className: className,
-        setTheme: function() {
-            setThemeCalled = true;
-        }
-    });
-
-    var view = new SomeView();
-
-    _.each(className.split(' '), function(_className) {
-        _className = joint.util.addClassNamePrefix(_className);
-        assert.ok(view.$el.hasClass(_className), 'has expected class name(s)');
-    });
-
-    assert.ok(setThemeCalled, 'should have executed setTheme() method');
-    assert.ok(_.keys(joint.mvc.views).length === 1, 'should add the instantiated view to the `joint.views` object');
-});
-
-test('options', function(assert) {
-
-    var SomeView = joint.mvc.View.extend({
-        options: {
-            option1: 'some value',
-            anotherOption: 150
-        }
-    });
-
-    var instanceOptions = {
-        option1: 'different value',
-        option2: true
     };
 
-    var view = new SomeView(instanceOptions);
+    hooks.beforeEach(resetViews);
+    hooks.afterEach(resetViews);
 
-    var expectedOptions = _.extend(
-        {},
-        joint.mvc.View.prototype.options,
-        SomeView.prototype.options,
-        instanceOptions
-    );
+    QUnit.test('constructor', function(assert) {
 
-    assert.deepEqual(view.options, expectedOptions, 'options should be inherited correctly');
-});
+        assert.ok(typeof joint.mvc.View === 'function', 'should be a function');
 
-test('options for an svg element', function(assert) {
+        var setThemeCalled = false;
+        var className = 'class-name1 class-name2';
 
-    var svgElementView = joint.mvc.View.extend({
-        tagName: 'g',
-        svgElement: true
-    });
-    var svgEl = new svgElementView();
+        var SomeView = joint.mvc.View.extend({
+            className: className,
+            setTheme: function() {
+                setThemeCalled = true;
+            }
+        });
 
-    assert.ok(svgEl.el, "element is created");
-    assert.equal(svgEl.el.tagName.toLowerCase(), "g", "creates the correct element");
-    assert.ok(svgEl.el instanceof SVGElement, "element is of the type SVGElement");
-});
+        var view = new SomeView();
 
-test('init()', function(assert) {
+        _.each(className.split(' '), function(_className) {
+            _className = joint.util.addClassNamePrefix(_className);
+            assert.ok(view.$el.hasClass(_className), 'has expected class name(s)');
+        });
 
-    var called = false;
-
-    var SomeView = joint.mvc.View.extend({
-        init: function() {
-            called = true;
-        }
+        assert.ok(setThemeCalled, 'should have executed setTheme() method');
+        assert.ok(_.keys(joint.mvc.views).length === 1, 'should add the instantiated view to the `joint.views` object');
     });
 
-    new SomeView();
+    QUnit.test('options', function(assert) {
 
-    assert.ok(called, 'should be executed when a new view is created');
-});
+        var SomeView = joint.mvc.View.extend({
+            options: {
+                option1: 'some value',
+                anotherOption: 150
+            }
+        });
 
-test('setTheme(theme)', function(assert) {
+        var instanceOptions = {
+            option1: 'different value',
+            option2: true
+        };
 
-    var theme = 'some-theme';
-    var defaultTheme = joint.mvc.View.prototype.defaultTheme;
-    var SomeView = joint.mvc.View.extend();
-    var view = new SomeView();
+        var view = new SomeView(instanceOptions);
 
-    view.setTheme(theme);
+        var expectedOptions = _.extend(
+            {},
+            joint.mvc.View.prototype.options,
+            SomeView.prototype.options,
+            instanceOptions
+        );
 
-    var themeClassName = SomeView.prototype.themeClassNamePrefix + theme;
-
-    assert.notEqual(defaultTheme, undefined, 'default theme is set');
-    assert.equal(view.theme, theme, 'should correctly set the theme for the view');
-    assert.ok(view.$el.hasClass(themeClassName) && !view.$el.hasClass(defaultTheme), 'view.$el should have correct theme class name');
-});
-
-test('render()', function(assert) {
-
-    assert.ok(typeof joint.mvc.View.prototype.render === 'function', 'should be a function');
-
-    var view = new joint.mvc.View();
-
-    assert.equal(view.render(), view, 'should return itself');
-});
-
-test('onRender()', function(assert) {
-
-    assert.ok(typeof joint.mvc.View.prototype.onRender === 'function', 'should be a function');
-
-    var called;
-
-    var SomeView = joint.mvc.View.extend({
-        onRender: function() {
-            called = true;
-        }
+        assert.deepEqual(view.options, expectedOptions, 'options should be inherited correctly');
     });
 
-    var view = new SomeView();
+    QUnit.test('options for an svg element', function(assert) {
 
-    called = false;
-    view.render();
-    assert.ok(called, 'should be called when render() is called');
-});
+        var svgElementView = joint.mvc.View.extend({
+            tagName: 'g',
+            svgElement: true
+        });
+        var svgEl = new svgElementView();
 
-test('classNamePrefix', function(assert) {
-
-    var className = 'custom-class-name';
-
-    var SomeView = joint.mvc.View.extend({
-        className: className
+        assert.ok(svgEl.el, "element is created");
+        assert.equal(svgEl.el.tagName.toLowerCase(), "g", "creates the correct element");
+        assert.ok(svgEl.el instanceof SVGElement, "element is of the type SVGElement");
     });
 
-    var view = new SomeView();
-    var defaultTheme = joint.mvc.View.prototype.defaultTheme;
-    var themeClassName = SomeView.prototype.themeClassNamePrefix + defaultTheme;
+    QUnit.test('init()', function(assert) {
 
-    assert.equal(view.$el.attr('class'), themeClassName + ' ' + joint.util.addClassNamePrefix(className));
-});
+        var called = false;
 
-//TODO
-// test('onRender is called once', function(assert) {
-//
-//     var onRenderSpy = sinon.spy();
-//     var a = joint.mvc.View.extend({
-//         onRender: function() {}
-//     });
-//
-//     var b = a.extend({
-//         onRender: onRenderSpy
-//     });
-//
-//     new b().render();
-//     assert.ok(onRenderSpy.calledOnce)
-// });
+        var SomeView = joint.mvc.View.extend({
+            init: function() {
+                called = true;
+            }
+        });
 
+        new SomeView();
 
-test('mvc.View.extend does not modify prototype', function(assert) {
+        assert.ok(called, 'should be executed when a new view is created');
+    });
 
-    var proto = {};
+    QUnit.test('setTheme(theme)', function(assert) {
 
-    var a = joint.mvc.View.extend(proto);
-    a.extend(proto);
+        var theme = 'some-theme';
+        var defaultTheme = joint.mvc.View.prototype.defaultTheme;
+        var SomeView = joint.mvc.View.extend();
+        var view = new SomeView();
 
-    assert.ok(proto.render === undefined);
+        view.setTheme(theme);
+
+        var themeClassName = SomeView.prototype.themeClassNamePrefix + theme;
+
+        assert.notEqual(defaultTheme, undefined, 'default theme is set');
+        assert.equal(view.theme, theme, 'should correctly set the theme for the view');
+        assert.ok(view.$el.hasClass(themeClassName) && !view.$el.hasClass(defaultTheme), 'view.$el should have correct theme class name');
+    });
+
+    QUnit.test('render()', function(assert) {
+
+        assert.ok(typeof joint.mvc.View.prototype.render === 'function', 'should be a function');
+
+        var called;
+
+        var SomeView = joint.mvc.View.extend({
+            render: function() {
+                called = true;
+            }
+        });
+
+        var view = new SomeView();
+        called = false;
+        var returnValue = view.render();
+
+        assert.ok(called, 'custom render method should be called');
+        assert.equal(returnValue, view, 'should return itself');
+    });
+
+    QUnit.test('onRender()', function(assert) {
+
+        assert.ok(typeof joint.mvc.View.prototype.onRender === 'function', 'should be a function');
+
+        var called;
+
+        var SomeView = joint.mvc.View.extend({
+            onRender: function() {
+                called = true;
+            }
+        });
+
+        var view = new SomeView();
+
+        called = false;
+        view.render();
+        assert.ok(called, 'should be called when render() is called');
+    });
+
+    QUnit.test('classNamePrefix', function(assert) {
+
+        var className = 'custom-class-name';
+
+        var SomeView = joint.mvc.View.extend({
+            className: className
+        });
+
+        var view = new SomeView();
+        var defaultTheme = joint.mvc.View.prototype.defaultTheme;
+        var themeClassName = SomeView.prototype.themeClassNamePrefix + defaultTheme;
+
+        assert.equal(view.$el.attr('class'), themeClassName + ' ' + joint.util.addClassNamePrefix(className));
+    });
+
+    QUnit.test('mvc.View.extend does not modify prototype or static properties objects', function(assert) {
+
+        var protoProps = {};
+        var staticProps = {};
+
+        var MyView = joint.mvc.View.extend(protoProps, staticProps);
+        MyView.extend(protoProps, staticProps);
+
+        assert.ok(_.isEmpty(protoProps), 'does not add any properties to prototype properties argument');
+        assert.ok(_.isEmpty(staticProps), 'does not add any properties to static properties argument');
+    });
 });
