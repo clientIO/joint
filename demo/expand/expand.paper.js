3e538409308fef57efbbb576795bee38ac04ba97
vladimir talas <vladimir.talas@gmail.com>
2017-08-17 09:33:30 +0200

    Demo: expand (#619)

---
+++ demo/expand/expand.paper.js
@@ -0,0 +1,91 @@
+joint.dia.ExpandPaper = joint.dia.Paper.extend({
+
+    initialize: function() {
+
+        joint.dia.Paper.prototype.initialize.apply(this, arguments);
+        this.listenTo(this.model, 'change:hidden', this.onHiddenChanged);
+    },
+
+    renderView: function(cell) {
+
+        var graph = this.model;
+        if (cell.isLink()) {
+            var source = cell.get('source');
+            var target = cell.get('target');
+
+            if (target && target.id) {
+                var targetCell = graph.getCell(target.id);
+                if (!targetCell.isVisible()) {
+                    cell.set('hidden', { silent: true });
+                }
+            }
+
+            if (source && source.id) {
+                var sourceCell = graph.getCell(source.id);
+                if (!sourceCell.isVisible()) {
+                    cell.set('hidden', { silent: true });
+                }
+            }
+        }
+
+        if (cell.isVisible()) {
+            joint.dia.Paper.prototype.renderView.apply(this, arguments);
+        }
+    },
+
+    onHiddenChanged: function(cell, hidden) {
+        var cellView = cell.findView(paper);
+        var graph = this.model;
+
+        if (cell.isLink()) {
+            var source = cell.get('source');
+            var target = cell.get('target');
+
+            if (target && target.id) {
+                var targetCell = graph.getCell(target.id);
+                targetCell.trigger('neighborHiddenChanged', target, hidden);
+            }
+
+            if (source && source.id) {
+                var sourceCell = graph.getCell(source.id);
+                sourceCell.trigger('neighborHiddenChanged', source, hidden);
+            }
+        }
+
+        // hide affected links before elements
+        if (cell.isElement() && hidden) {
+            this.processLinksOnElementToggle(cell, hidden);
+        }
+
+        if (hidden && cellView) {
+            this.removeView(cell);
+        }
+
+        else if (!hidden && !cellView) {
+            joint.dia.Paper.prototype.renderView.call(this, cell);
+        }
+
+        // show affected links after elements are rendered
+        if (cell.isElement() && !hidden) {
+            this.processLinksOnElementToggle(cell, hidden);
+        }
+    },
+
+    processLinksOnElementToggle: function(element, hidden) {
+
+        var graph = this.model;
+        var links = graph.getConnectedLinks(element);
+
+        _.each(links, function(link) {
+
+            var oppositeCell;
+            var r = link.resolveOrientation(element);
+
+            oppositeCell = graph.getCell(r.oppositeEnd.id);
+
+            if (!r.oppositeEnd.id || oppositeCell && oppositeCell.isVisible()) {
+                hidden ? link.hide() : link.show();
+            }
+        }, this);
+    }
+});
---
+++ demo/expand/shapes.js
@@ -0,0 +1,124 @@
+joint.dia.Link.define('toggable.Link', null, {
+
+    hide: function() {
+        this.set('hidden', true)
+    },
+
+    show: function() {
+        this.set('hidden', false)
+    },
+
+    isVisible: function() {
+        return !this.get('hidden');
+    },
+
+    resolveOrientation: function(element) {
+
+        var source = this.get('source');
+        var target = this.get('target');
+        var result;
+
+        if (source && source.id !== element.id) {
+            result = {
+                oppositeEnd: source,
+                currentEnd: target
+            };
+        }
+
+        if (target && target.id !== element.id) {
+            result = {
+                oppositeEnd: target,
+                currentEnd: source
+            }
+        }
+
+        return result;
+    }
+});
+
+joint.dia.Element.define('toggable.Element', {
+    hidden: false
+}, {
+    options: {
+        onPortExpand: function(cell, portId, expand) {
+            console.log('on port expand event', arguments);
+        }
+    },
+
+    initialize: function() {
+
+        joint.dia.Element.prototype.initialize.apply(this, arguments);
+        this.on('neighborHiddenChanged', this.neighborHiddenChanged);
+    },
+
+    neighborHiddenChanged: function(connection, hidden) {
+
+        if (connection.port) {
+            this.neighborHiddenChangedByPort(connection.port, hidden);
+        }
+    },
+
+    hide: function() {
+        this.set('hidden', true);
+    },
+
+    show: function() {
+        this.set('hidden', false);
+    },
+
+    isVisible: function() {
+        return !this.get('hidden');
+    },
+
+    isPortCollapsed: function(portId) {
+        var collapsedList = this.get('collapsed') || {};
+        return collapsedList[portId] > 0;
+    },
+
+    expandPort: function(portId) {
+
+        if (portId) {
+
+            if (this.isPortCollapsed(portId)) {
+                _.each(this.graph.getConnectedLinks(this), function(link) {
+                    var orientedEnds = link.resolveOrientation(this);
+                    if (orientedEnds && orientedEnds.currentEnd.port === portId && orientedEnds.oppositeEnd.id) {
+                        this.graph.getCell(orientedEnds.oppositeEnd.id).show();
+                    }
+                }, this);
+            }
+        }
+    },
+
+    neighborHiddenChangedByPort: function(portId, hidden) {
+
+        var expand = !hidden;
+        var collapsedList = _.clone(this.get('collapsed')) || {};
+
+        if (expand) {
+            // expand
+
+            if (_.isFinite(collapsedList[portId])) {
+                collapsedList[portId]--;
+            }
+
+            if (collapsedList[portId] <= 0) {
+                delete collapsedList[portId];
+                this.portProp(portId, 'collapsed', false);
+                this.options.onPortExpand(this, portId, expand);
+            }
+        } else {
+            // collapse
+
+            if (!collapsedList[portId]) {
+                collapsedList[portId] = 1;
+                this.portProp(portId, 'collapsed', true);
+                this.options.onPortExpand(this, portId, expand);
+            } else {
+                collapsedList[portId]++;
+            }
+        }
+
+        this.set('collapsed', collapsedList);
+    }
+});
