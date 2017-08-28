b09b7240487e217603540fefe872dc6106033d7b
Roman Bruckner <bruckner.roman@gmail.com>
2017-08-28 07:31:50 +0200

    dia.Paper: `moveThreshold` option (#621)

--- src/joint.dia.paper.js
+++ src/joint.dia.paper.js
@@ -133,6 +133,9 @@ joint.dia.Paper = joint.mvc.View.extend({
         // Allowed number of mousemove events after which the pointerclick event will be still triggered.
         clickThreshold: 0,
 
+        // Number of required mousemove events before the first pointermove event will be triggered.
+        moveThreshold: 0,
+
         // The namespace, where all the cell views are defined.
         cellViewNamespace: joint.shapes,
 
@@ -1273,17 +1276,20 @@ joint.dia.Paper = joint.mvc.View.extend({
 
     pointermove: function(evt) {
 
-        if (this.sourceView) {
+        var view = this.sourceView;
+        if (view) {
 
             evt.preventDefault();
-            evt = joint.util.normalizeEvent(evt);
 
             // Mouse moved counter.
-            this._mousemoved++;
+            var mousemoved = ++this._mousemoved;
+            if (mousemoved > this.options.moveThreshold) {
 
-            var localPoint = this.snapToGrid({ x: evt.clientX, y: evt.clientY });
+                evt = joint.util.normalizeEvent(evt);
 
-            this.sourceView.pointermove(evt, localPoint.x, localPoint.y);
+                var localPoint = this.snapToGrid({ x: evt.clientX, y: evt.clientY });
+                view.pointermove(evt, localPoint.x, localPoint.y);
+            }
         }
     },
 
