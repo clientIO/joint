dbc0e158c50f2c8224e3af48d10f17d0dafdd665
vladimir talas <lades@client.io>
2017-09-06 07:29:36 +0200

    fsa

--- plugins/shapes/joint.shapes.fsa.js
+++ plugins/shapes/joint.shapes.fsa.js
@@ -1,65 +1,44 @@
-joint.shapes.fsa = {};
-
-joint.shapes.fsa.State = joint.shapes.basic.Circle.extend({
-    defaults: _.defaultsDeep({
-        type: 'fsa.State',
-        attrs: {
-            circle: { 'stroke-width': 3 },
-            text: { 'font-weight': '800' }
-        }
-    }, joint.shapes.basic.Circle.prototype.defaults)
+joint.shapes.basic.Circle.define('fsa.State', {
+    attrs: {
+        circle: { 'stroke-width': 3 },
+        text: { 'font-weight': '800' }
+    }
 });
 
-joint.shapes.fsa.StartState = joint.dia.Element.extend({
-
+joint.dia.Element.define('fsa.StartState', {
+    size: { width: 20, height: 20 },
+    attrs: {
+        circle: {
+            transform: 'translate(10, 10)',
+            r: 10,
+            fill: '#000000'
+        }
+    }
+}, {
     markup: '<g class="rotatable"><g class="scalable"><circle/></g></g>',
-
-    defaults: _.defaultsDeep({
-
-        type: 'fsa.StartState',
-        size: { width: 20, height: 20 },
-        attrs: {
-            circle: {
-                transform: 'translate(10, 10)',
-                r: 10,
-                fill: '#000000'
-            }
-        }
-
-    }, joint.dia.Element.prototype.defaults)
 });
 
-joint.shapes.fsa.EndState = joint.dia.Element.extend({
+joint.dia.Element.define('fsa.EndState', {
+    size: { width: 20, height: 20 },
+    attrs: {
+        '.outer': {
+            transform: 'translate(10, 10)',
+            r: 10,
+            fill: '#ffffff',
+            stroke: '#000000'
+        },
 
+        '.inner': {
+            transform: 'translate(10, 10)',
+            r: 6,
+            fill: '#000000'
+        }
+    }
+}, {
     markup: '<g class="rotatable"><g class="scalable"><circle class="outer"/><circle class="inner"/></g></g>',
-
-    defaults: _.defaultsDeep({
-
-        type: 'fsa.EndState',
-        size: { width: 20, height: 20 },
-        attrs: {
-            '.outer': {
-                transform: 'translate(10, 10)',
-                r: 10,
-                fill: '#ffffff',
-                stroke: '#000000'
-            },
-
-            '.inner': {
-                transform: 'translate(10, 10)',
-                r: 6,
-                fill: '#000000'
-            }
-        }
-
-    }, joint.dia.Element.prototype.defaults)
 });
 
-joint.shapes.fsa.Arrow = joint.dia.Link.extend({
-
-    defaults: _.defaultsDeep({
-        type: 'fsa.Arrow',
-        attrs: { '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' }},
-        smooth: true
-    }, joint.dia.Link.prototype.defaults)
+joint.dia.Link.define('fsa.Arrow', {
+    attrs: { '.marker-target': { d: 'M 10 0 L 0 5 L 10 10 z' } },
+    smooth: true
 });
