e0de97c513374b7c929eaba7233f5f79d00a8de6
vladimir talas <lades@client.io>
2017-09-06 14:23:54 +0200

    remove private methods

--- types/joint.d.ts
+++ types/joint.d.ts
@@ -515,8 +515,6 @@ export namespace dia {
 
         isDefined(defId: string): boolean;
 
-        linkAllowed(linkViewOrModel: LinkView | Link): boolean;
-
         localToClientPoint(x: number | g.Point, y?: number): g.Point;
 
         localToClientRect(x: number | g.Rect, y?: number, width?: number, height?: number): g.Rect;
@@ -554,9 +552,9 @@ export namespace dia {
 
         setDimensions(width: number, height: number): void;
 
-        setGrid(drawGrid: GridOptions | GridOptions[]): this;
+		
 
-        setGridSize(gridSize: number): this;
+		setGridSize(gridSize: number): this;
 
         setInteractivity(value: any): void;
 
@@ -724,8 +722,8 @@ export namespace dia {
 
         addVertex(vertex: Point): number;
 
-        getPointAtLength(length: number): g.Point; // Marked as public api in source but not in the documents
-        createWatcher(endType: { id: string }): (link: Link, end?: {id: string}) => this;
+		getPointAtLength(length: number): g.Point; // Marked as public api in source but not in the documents
+		
 
         update(model: Cell, attributes: object, options?: object): this;
 
