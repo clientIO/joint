bc7ede3f41a329dced07ea8e9ef28afd82616b69
vladimir talas <lades@client.io>
2017-09-09 09:50:10 +0200

    _.isEqual

--- src/joint.dia.cell.js
+++ src/joint.dia.cell.js
@@ -50,14 +50,14 @@ joint.dia.Cell = Backbone.Model.extend({
 
                     _.each(value, function(value2, name2) {
 
-                        if (!defaultAttr || !defaultAttr[name] || !_.isEqual(defaultAttr[name][name2], value2)) {
+                        if (!defaultAttr || !defaultAttr[name] || !joint.util.isEqual(defaultAttr[name][name2], value2)) {
 
                             finalAttrs[selector] = finalAttrs[selector] || {};
                             (finalAttrs[selector][name] || (finalAttrs[selector][name] = {}))[name2] = value2;
                         }
                     });
 
-                } else if (!defaultAttr || !_.isEqual(defaultAttr[name], value)) {
+                } else if (!defaultAttr || !joint.util.isEqual(defaultAttr[name], value)) {
                     // `value` is not an object, default attribute for such a selector does not exist
                     // or it is different than the attribute value set on the model.
 
@@ -597,7 +597,7 @@ joint.dia.Cell = Backbone.Model.extend({
 
         _(this._transitionIds).keys().filter(pathArray && function(key) {
 
-            return _.isEqual(pathArray, key.split(delim).slice(0, pathArray.length));
+            return joint.util.isEqual(pathArray, key.split(delim).slice(0, pathArray.length));
 
         }).each(function(key) {
 
