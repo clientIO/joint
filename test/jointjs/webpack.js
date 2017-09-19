78fb5deaadab878d9c20952a1794e30919891e2c
vladimir talas <lades@client.io>
2017-09-19 15:02:51 +0200

    qunit vs webpack

--- test/jointjs/webpack.js
+++ test/jointjs/webpack.js
@@ -1,13 +1,15 @@
-module('Webpack');
+QUnit.module('Webpack', function() {
 
-test('sanity checks for webpack bundle file', function(assert) {
+    QUnit.test('sanity checks for webpack bundle file', function(assert) {
 
-    assert.ok(typeof joint !== 'undefined', 'Joint object should be defined');
-    assert.ok(typeof joint.dia === 'object', 'Joint object should have "dia" object');
+        assert.ok(typeof joint !== 'undefined', 'Joint object should be defined');
+        assert.ok(typeof joint.dia === 'object', 'Joint object should have "dia" object');
 
-    var paper = new joint.dia.Paper();
+        var paper = new joint.dia.Paper();
 
-    assert.ok(paper instanceof Backbone.View, 'A new dia.Paper object can be initialized');
+        assert.ok(paper instanceof Backbone.View, 'A new dia.Paper object can be initialized');
 
-    paper.remove();
+        paper.remove();
+    });
 });
+
