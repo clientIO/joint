b5cb08da2cb6178e5b91c13bf314c8fdfc7b0bbd
Roman Bruckner <bruckner.roman@gmail.com>
2017-09-19 11:56:01 +0200

    update

--- tutorials/js/multiple-papers-and-views.js
+++ tutorials/js/multiple-papers-and-views.js
@@ -1,5 +1,27 @@
 (function multiplePapers(joint, V) {
 
+    function createElementPreview(color) {
+        return joint.dia.ElementView.extend({
+            renderMarkup: function() {
+                // Always render a grayish rectangle
+                this.velBody = V('rect', {
+                    fill: color,
+                    stroke: 'gray'
+                }).appendTo(this.vel.empty());
+            },
+            resize: function() {
+                // Always set the rectangle size to the size of the model
+                this.velBody.attr(this.model.size());
+            },
+            update: function() {
+                // Noop: we simply ignore all attrs attributes.
+            }
+        });
+    }
+
+    var Preview = createElementPreview('lightgray');
+    var OtherPreview = createElementPreview('lightblue');
+
     var graph = new joint.dia.Graph;
 
     var paper1 = new joint.dia.Paper({
@@ -9,29 +31,21 @@
         model: graph
     });
 
-    var ElementPreview = joint.dia.ElementView.extend({
-        renderMarkup: function() {
-            // Always render a grayish rectangle
-            this.velBody = V('rect', {
-                fill: 'lightgray',
-                stroke: 'gray'
-            }).appendTo(this.vel.empty());
-        },
-        resize: function() {
-            // Always set the rectangle size to the size of the model
-            this.velBody.attr(this.model.size());
-        },
-        update: function() {
-            // Noop: we simply ignore all attrs attributes.
-        }
-    });
-
     var paper2 = new joint.dia.Paper({
         el: document.getElementById('paper-multiple-papers-and-views-small'),
         width: 600,
         height: 100,
         model: graph,
-        elementView: ElementPreview
+        elementView: Preview
+        /* An example how to use a different view based on the model
+        elementView: function(element) {
+            switch (element.get('type')) {
+                case 'basic.Rect': return Preview;
+                case 'basic.Circle': return OtherPreview;
+                default: return null; // use default jointjs view otherwise
+            }
+        }
+        */
     });
 
     var el1 = new joint.shapes.basic.Rect({
