cfd7f386e6dccd40e510ce5069f1acf022157b52
vladimir talas <lades@client.io>
2017-09-19 14:29:03 +0200

    qunit 2.4

--- test/jointjs/browserify.js
+++ test/jointjs/browserify.js
@@ -1,9 +1,11 @@
-module('Browserify');
+QUnit.module('Browserify', function() {
 
-test('sanity checks for browserify bundle file', function(assert) {
+    QUnit.test('sanity checks for browserify bundle file', function(assert) {
 
-    assert.ok(typeof joint !== 'undefined', 'Joint object should be defined');
-    assert.ok(typeof joint.dia === 'object', 'Joint object should have "dia" object');
-    assert.ok(typeof joint.dia.Graph === 'function', 'joint.dia.Graph should be a function');
-    assert.ok(typeof joint.dia.Paper === 'function', 'joint.dia.Paper should be a function');
+        assert.ok(typeof joint !== 'undefined', 'Joint object should be defined');
+        assert.ok(typeof joint.dia === 'object', 'Joint object should have "dia" object');
+        assert.ok(typeof joint.dia.Graph === 'function', 'joint.dia.Graph should be a function');
+        assert.ok(typeof joint.dia.Paper === 'function', 'joint.dia.Paper should be a function');
+    });
 });
+
