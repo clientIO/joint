93c351f689dc18c346246896ddee35022d53e168
vladimir talas <lades@client.io>
2017-09-27 07:13:53 +0200

    startsWith

--- plugins/polyfills/string.js
+++ plugins/polyfills/string.js
@@ -12,3 +12,9 @@ if (!String.prototype.includes) {
         }
     };
 }
+
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+        return this.substr(position || 0, searchString.length) === searchString;
+    };
+}
