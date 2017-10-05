d59d5247d5efd4f9faba6e96d3b86b56fe48ffa9
Roman Bruckner <bruckner.roman@gmail.com>
2017-10-05 10:50:54 +0200

    layout.DirectedGraph: make sure parent cells are in the graph (#686)
    * layout.DirectedGraph: make sure parent cells are in the graph
    
    * test

--- test/jointjs/plugins/layout/DirectedGraph.js
+++ test/jointjs/plugins/layout/DirectedGraph.js
@@ -122,6 +122,55 @@ QUnit.module('layout.DirectedGraph', function(hooks) {
             assert.deepEqual(_.pick(elements[3].position(), 'x', 'y'), { x: 400, y: 50 });
         });
 
+        QUnit.test('an array of embedded cells without the parent', function(assert) {
+
+            var elements = [
+                new joint.shapes.basic.Rect({
+                    position: { x: 0, y: 0 },
+                    size: { width: 60, height: 100 }
+                }),
+                new joint.shapes.basic.Rect({
+                    position: { x: 200, y: 0 },
+                    size: { width: 40, height: 80 }
+                }),
+                new joint.shapes.basic.Rect({
+                    position: { x: 300, y: 20 },
+                    size: { width: 20, height: 30 }
+                }),
+                new joint.shapes.basic.Rect({
+                    position: { x: 400, y: 50 },
+                    size: { width: 30, height: 20 }
+                })
+            ];
+
+            elements[0].embed(elements[1]);
+            elements[0].embed(elements[2]);
+            elements[0].embed(elements[3]);
+
+            var links = [
+                new joint.dia.Link({ source: { id: elements[1].id }, target: { id: elements[2].id }}),
+                new joint.dia.Link({ source: { id: elements[2].id }, target: { id: elements[3].id }})
+            ];
+
+            var cells = elements.concat(links);
+
+            graph.resetCells(cells);
+
+            joint.layout.DirectedGraph.layout([
+                elements[1],
+                elements[2],
+                elements[3],
+                links[0],
+                links[1]
+            ]);
+
+            assert.deepEqual(_.pick(elements[0].position(), 'x', 'y'), { x: 0, y: 0 });
+            assert.deepEqual(_.pick(elements[1].position(), 'x', 'y'), { x: 0, y: 0 });
+            assert.deepEqual(_.pick(elements[2].position(), 'x', 'y'), { x: 10, y: 130 });
+            assert.deepEqual(_.pick(elements[3].position(), 'x', 'y'), { x: 5, y: 210 });
+        });
+
+
         QUnit.test('should return a rectangle representing the graph bounding box', function(assert) {
 
             var bbox;
