d655abb5a9b01f8f88ff88256acf53357b13e5bb
Roman Bruckner <bruckner.roman@gmail.com>
2018-01-19 10:47:46 +0100

    dia.ElementView: allow element's rotation without rotatable group (#789)

--- test/jointjs/elementView.js
+++ test/jointjs/elementView.js
@@ -84,4 +84,43 @@ QUnit.module('elementView', function(hooks) {
             elementView.model.resize(200, 300, { passed: true });
         });
     });
+
+    QUnit.module('no rotatable group and no scalable group', function(hooks) {
+
+        hooks.beforeEach(function() {
+
+            elementView.model.set({
+                markup: '<rect class="body"/><text class="label"/>'
+            }).attr({
+                '.body': {
+                    'ref-width': 1,
+                    'ref-height': 1
+                }
+            });
+        });
+
+        QUnit.test('un-rotated elementView should update the size when model is resized', function(assert) {
+
+            elementView.model.rotate(0).resize(100, 200).translate(100, 100);
+
+            assert.checkBboxApproximately(1, elementView.getBBox(), {
+                x: 100,
+                y: 100,
+                width: 100,
+                height: 200
+            });
+        });
+
+        QUnit.test('rotated elementView should update the size when model is resized', function(assert) {
+
+            elementView.model.rotate(90).resize(100, 200).translate(100, 100);
+
+            assert.checkBboxApproximately(1, elementView.getBBox(), {
+                x: 50,
+                y: 150,
+                width: 200,
+                height: 100
+            });
+        });
+    });
 });
