19f97844f18602ecd5a9c91fff941615dfe73860
Zbynek Stara <zbynek.stara.internet@hotmail.com>
2018-01-20 15:37:51 +0100

    utils: Download (#798)

--- src/core.js
+++ src/core.js
@@ -558,6 +558,77 @@ var joint = {
             return lines.join('\n');
         },
 
+        // Download `blob` as file with `fileName`.
+        // Does not work in IE9.
+        downloadBlob: function(blob, fileName) {
+
+            if (window.navigator.msSaveBlob) { // requires IE 10+
+                // pulls up a save dialog
+                window.navigator.msSaveBlob(blob, fileName);
+
+            } else { // other browsers
+                // downloads directly in Chrome and Safari
+
+                // presents a save/open dialog in Firefox
+                // Firefox bug: `from` field in save dialog always shows `from:blob:`
+                // https://bugzilla.mozilla.org/show_bug.cgi?id=1053327
+
+                var url = window.URL.createObjectURL(blob);
+                var link = document.createElement('a');
+
+                link.href = url;
+                link.download = fileName;
+                document.body.appendChild(link);
+
+                link.click();
+
+                document.body.removeChild(link);
+                window.URL.revokeObjectURL(url); // mark the url for garbage collection
+            }
+        },
+
+        // Download `dataUri` as file with `fileName`.
+        // Does not work in IE9.
+        downloadDataUri: function(dataUri, fileName) {
+
+            var blob = joint.util.dataUriToBlob(dataUri);
+            joint.util.downloadBlob(blob, fileName);
+        },
+
+        // Convert an uri-encoded data component (possibly also base64-encoded) to a blob.
+        dataUriToBlob: function(dataUri) {
+
+            // first, make sure there are no newlines in the data uri
+            dataUri = dataUri.replace(/\s/g, '');
+            dataUri = decodeURIComponent(dataUri);
+
+            var firstCommaIndex = dataUri.indexOf(','); // split dataUri as `dataTypeString`,`data`
+
+            var dataTypeString = dataUri.slice(0, firstCommaIndex); // e.g. 'data:image/jpeg;base64'
+            var mimeString = dataTypeString.split(':')[1].split(';')[0]; // e.g. 'image/jpeg'
+
+            var data = dataUri.slice(firstCommaIndex + 1);
+            var decodedString;
+            if (dataTypeString.indexOf('base64') >= 0) { // data may be encoded in base64
+                decodedString = atob(data); // decode data
+            } else {
+                decodedString = data;
+            }
+
+            // write the bytes of the string to a typed array
+            var ia = new window.Uint8Array(decodedString.length);
+            for (var i = 0; i < decodedString.length; i++) {
+                ia[i] = decodedString.charCodeAt(i);
+            }
+
+            return new Blob([ia], { type: mimeString }); // return the typed array as Blob
+        },
+
+        // Read an image at `url` and return it as base64-encoded data uri.
+        // The mime type of the image is inferred from the `url` file extension.
+        // If data uri is provided as `url`, it is returned back unchanged.
+        // `callback` is a method with `err` as first argument and `dataUri` as second argument.
+        // Works with IE9.
         imageToDataUri: function(url, callback) {
 
             if (!url || url.substr(0, 'data:'.length) === 'data:') {
@@ -575,7 +646,7 @@ var joint = {
                 }, 0);
             }
 
-            // chrome IE10 IE11
+            // chrome, IE10+
             var modernHandler = function(xhr, callback) {
 
                 if (xhr.status === 200) {
@@ -595,7 +666,6 @@ var joint = {
                 } else {
                     callback(new Error('Failed to load image ' + url));
                 }
-
             };
 
             var legacyHandler = function(xhr, callback) {
@@ -608,7 +678,6 @@ var joint = {
                     }
                     return c.join('');
                 };
-
 
                 if (xhr.status === 200) {
 
