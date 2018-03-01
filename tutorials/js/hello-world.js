ae0069c60ea8540e869ac9c19e3864299239df85
zbynekstara <zbynek.stara.internet@hotmail.com>
2018-03-01 17:43:42 +0100

    4 pages of basic tutorial

--- tutorials/js/hello-world.js
+++ tutorials/js/hello-world.js
@@ -1,32 +1,41 @@
 (function basic() {
 
     var graph = new joint.dia.Graph;
+
     var paper = new joint.dia.Paper({
-        el: $('#paper-basic'),
+        el: document.getElementById('paper-basic'),
+        model: graph,
         width: 600,
         height: 100,
-        model: graph,
         gridSize: 1
     });
-    var rect = new joint.shapes.basic.Rect({
-        position: { x: 100, y: 30 },
-        size: { width: 100, height: 30 },
-        attrs: { rect: { fill: 'blue' }, text: { text: 'my box', fill: 'white' } }
+
+    var rect = new joint.shapes.standard.Rectangle();
+    rect.position(100, 30);
+    rect.resize(100, 30);
+    rect.attr({
+        body: {
+            fill: 'blue'
+        },
+        label: {
+            text: 'Hello',
+            style: {
+                fill: 'white'
+            }
+        }
     });
+
     var rect2 = rect.clone();
-    rect2.translate(300);
-    
-    var link = new joint.dia.Link({
-        source: { id: rect.id },
-        target: { id: rect2.id }
-    });
+    rect2.translate(300, 0);
+    rect2.attr('label/text', 'World!');
+
+    var link = new joint.shapes.standard.Link();
+    link.source({ id: rect.id });
+    link.target({ id: rect2.id });
+
     graph.addCells([rect, rect2, link]);
 
     graph.on('all', function(eventName, cell) {
         console.log(arguments);
-    });
-
-    rect.on('change:position', function(element) {
-        console.log(element.id, ':', element.get('position'));
     });
 }());
