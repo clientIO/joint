5ae238e82beea4b07d9228ffdd22f8e40767059c
zbynekstara <zbynek.stara.internet@hotmail.com>
2018-04-19 12:06:05 +0200

    custom button

--- tutorials/js/link-tools-custom-button.js
+++ tutorials/js/link-tools-custom-button.js
@@ -66,26 +66,26 @@
     link.addTo(graph);
 
     joint.linkTools.InfoButton = joint.linkTools.Button.extend({
-        children: [{
-            tagName: 'circle',
-            selector: 'button',
-            attributes: {
-                'r': 7,
-                'fill': '#001DFF',
-                'cursor': 'pointer'
-            }
-        }, {
-            tagName: 'path',
-            selector: 'icon',
-            attributes: {
-                'd': 'M -2 4 2 4 M 0 3 0 0 M -2 -1 1 -1 M -1 -4 1 -4',
-                'fill': 'none',
-                'stroke': '#FFFFFF',
-                'stroke-width': 2,
-                'pointer-events': 'none'
-            }
-        }],
         options: {
+            markup: [{
+                tagName: 'circle',
+                selector: 'button',
+                attributes: {
+                    'r': 7,
+                    'fill': '#001DFF',
+                    'cursor': 'pointer'
+                }
+            }, {
+                tagName: 'path',
+                selector: 'icon',
+                attributes: {
+                    'd': 'M -2 4 2 4 M 0 3 0 0 M -2 -1 1 -1 M -1 -4 1 -4',
+                    'fill': 'none',
+                    'stroke': '#FFFFFF',
+                    'stroke-width': 2,
+                    'pointer-events': 'none'
+                }
+            }],
             distance: 60,
             offset: 0,
             action: function(evt) {
