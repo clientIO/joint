278f9960ba6a1c92c79cab6fe8e7f9e242d2ffbe
Roman Bruckner <bruckner.roman@gmail.com>
2018-06-20 15:27:04 +0200

    Tutorials: improve connecting by dropping tutorial (#905)

--- tutorials/js/connecting-by-dropping.js
+++ tutorials/js/connecting-by-dropping.js
@@ -1,49 +1,72 @@
 (function() {
 
     var graph = new joint.dia.Graph;
-    var paper = new joint.dia.Paper({ el: $('#paper-connection-by-dropping'), width: 650, height: 200, gridSize: 1, model: graph });
+    var paper = new joint.dia.Paper({
+        el: document.getElementById('paper-connection-by-dropping'),
+        width: 650,
+        height: 200,
+        model: graph
+    });
 
-    var el1 = new joint.shapes.basic.Rect({
-        position: { x: 50, y: 50 }, size: { width: 100, height: 40 },
+    var el1 = new joint.shapes.standard.Rectangle({
+        position: { x: 50, y: 50 },
+        size: { width: 100, height: 40 },
         attrs: {
-            rect: {
-                'stroke-width': '5',
-                'stroke-opacity': .7,
+            body: {
+                strokeWidth: 5,
+                strokeOpacity: .7,
                 stroke: 'black',
                 rx: 3,
                 ry: 3,
                 fill: 'lightgray',
-                'fill-opacity': .5
+                fillOpacity: .5
             },
-            text: { text: 'Drop me over B', 'font-size': 10, style: { 'text-shadow': '1px 1px 1px lightgray' } }
-        }
-    });
-    graph.addCells([el1, el1.clone().translate(200, 50).attr('text/text', 'B')]);
-
-// Here is the real deal. Listen on cell:pointerup and link to an element found below.
-    paper.on('cell:pointerup', function(cellView, evt, x, y) {
-
-        // Find the first element below that is not a link nor the dragged element itself.
-        var elementBelow = graph.get('cells').find(function(cell) {
-            if (cell instanceof joint.dia.Link) return false; // Not interested in links.
-            if (cell.id === cellView.model.id) return false; // The same element as the dropped one.
-            if (cell.getBBox().containsPoint(g.point(x, y))) {
-                return true;
+            label: {
+                text: 'Drop me over B',
+                fontSize: 10,
+                style: { 'text-shadow': '1px 1px 1px lightgray' }
             }
-            return false;
-        });
-
-        // If the two elements are connected already, don't
-        // connect them again (this is application specific though).
-        if (elementBelow && !_.contains(graph.getNeighbors(elementBelow), cellView.model)) {
-
-            graph.addCell(new joint.dia.Link({
-                source: { id: cellView.model.id }, target: { id: elementBelow.id },
-                attrs: { '.marker-source': { d: 'M 10 0 L 0 5 L 10 10 z' } }
-            }));
-            // Move the element a bit to the side.
-            cellView.model.translate(-200, 0);
         }
     });
 
+    var el2 = el1.clone().translate(200, 50).attr('label/text', 'B');
+
+    graph.addCells([el1, el2]);
+
+    paper.on({
+
+        'element:pointerdown': function(elementView, evt) {
+
+            evt.data = elementView.model.position();
+        },
+
+        'element:pointerup': function(elementView, evt, x, y) {
+
+            var coordinates = new g.Point(x, y);
+            var elementAbove = elementView.model;
+            var elementBelow = this.model.findModelsFromPoint(coordinates).find(function(el) {
+                return (el.id !== elementAbove.id);
+            });
+
+            // If the two elements are connected already, don't
+            // connect them again (this is application-specific though).
+            if (elementBelow && graph.getNeighbors(elementBelow).indexOf(elementAbove) === -1) {
+
+                // Move the element to the position before dragging.
+                elementAbove.position(evt.data.x, evt.data.y);
+
+                // Create a connection between elements.
+                var link = new joint.shapes.standard.Link();
+                link.source(elementAbove);
+                link.target(elementBelow);
+                link.addTo(graph);
+
+                // Add remove button to the link.
+                var tools = new joint.dia.ToolsView({
+                    tools: [new joint.linkTools.Remove()]
+                });
+                link.findView(this).addTools(tools);
+            }
+        }
+    });
 }());
