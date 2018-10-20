7b0c52259b831bf861aba5ccc92c9a6979b56185
Roman Bruckner <bruckner.roman@gmail.com>
2018-10-20 22:59:53 +0200

    update

--- demo/nested2.js
+++ demo/nested2.js
@@ -1,46 +1,51 @@
 var graph = new joint.dia.Graph;
-var paper = new joint.dia.Paper({
-    el: $('#easel'),
+new joint.dia.Paper({
+    el: document.getElementById('easel'),
     width: 600,
     height: 200,
     gridSize: 1,
     model: graph
 });
-var parent = new joint.shapes.basic.Rect({
-    position: {x: 100,y: 30},
-    size: {width: 100,height: 30},
-    attrs: {rect: {fill: 'blue'},
-            text: {text: 'parent',fill: 'white'}}
+var parent = new joint.shapes.standard.Rectangle({
+    position: { x: 100, y: 30 },
+    size: { width: 100, height: 30 },
+    attrs: {
+        body: { fill: 'blue' },
+        label: { text: 'parent', fill: 'white' }
+    }
 });
-var child = new joint.shapes.basic.Rect({
-    position: {x: 70,y: 130},
-    size: {width: 70,height: 30},
-    attrs: {rect: {fill: 'lightgreen',rx: 5,ry: 5},
-            text: {text: 'child',fill: 'white'}}
+var child = new joint.shapes.standard.Rectangle({
+    position: { x: 70, y: 130 },
+    size: { width: 70, height: 30 },
+    attrs: {
+        body: { fill: 'lightgreen', rx: 5, ry: 5 },
+        label: { text: 'child', fill: 'white' }
+    }
 });
-link = new joint.dia.Link({
-    source: {id:parent.id},
-    target: {id:child.id}})
-parent.embed(child);
 var child2 = child.clone().translate(100);
-var link2 = new joint.dia.Link({
-    source: {id: parent.id},
-    target: {id: child2.id},
-    vertices: [{x: 210,y: 75}, {x: 190,y: 105}]
+var link = new joint.shapes.standard.Link({
+    source: { id: parent.id },
+    target: { id: child.id }
 });
+var link2 = new joint.shapes.standard.Link({
+    source: { id: parent.id },
+    target: { id: child2.id },
+    vertices: [{ x: 210, y: 75 }, { x: 190, y: 105 }]
+});
+parent.embed(child);
 parent.embed(child2);
+graph.addCells([parent, child, child2, link, link2]);
 
-$("#button").click(function () {
-    switch ($("#button").text().split(' ')[0]) {
+$('#button').click(function() {
+    switch ($('#button').text().split(' ')[0]) {
         case 'embed':
             parent.embed(link);
             parent.embed(link2);
-            $("#button").text('unembed links');
+            $('#button').text('unembed links');
             break;
         case 'unembed':
             parent.unembed(link);
             parent.unembed(link2);
-            $("#button").text('embed links');
-    };
+            $('#button').text('embed links');
+    }
 });
-graph.addCells([parent, child, child2, link, link2]);
--- demo/sketched.js
+++ demo/sketched.js
@@ -1,58 +1,65 @@
 var graph = new joint.dia.Graph;
-var paper = new joint.dia.Paper({ el: $('#paper'), width: 650, height: 400, model: graph });
+var paper = new joint.dia.Paper({
+    el: document.getElementById('paper'),
+    width: 650,
+    height: 400,
+    model: graph
+});
 
-var rb = new joint.shapes.basic.Rect({
+var rb = new joint.shapes.standard.Rectangle({
     position: { x: 50, y: 50 },
     size: { width: 120, height: 40 },
     attrs: {
-        text: { text: 'My Rectangle' },
-        rect: {
-            'stroke-width': 3,
+        label: {
+            text: 'My Rectangle'
+        },
+        body: {
+            strokeWidth: 3,
             stroke: '#7C68FC',
             fill: '#FEB663',
-            'fill-opacity': .8,
+            fillOpacity: .8,
             filter: 'url(#sketched-filter)'
         }
     }
 });
-graph.addCell(rb);
 
-var cb = new joint.shapes.basic.Circle({
+var cb1 = new joint.shapes.standard.Circle({
     position: { x: 300, y: 70 },
     size: { width: 100, height: 100 },
     attrs: {
-        text: { text: 'My Circle' },
-        circle: {
-            'stroke-width': 3,
+        label: {
+            text: 'My Circle'
+        },
+        body: {
+            strokeWidth: 3,
             stroke: '#31D0C6',
             fill: '#FEB663',
-            'fill-opacity': .8,
+            fillOpacity: .8,
             filter: 'url(#sketched-filter)'
         }
     }
 });
-graph.addCell(cb);
 
-var l1 = new joint.dia.Link({
+var cb2 = cb1.clone().attr('label/text', 'My Circle 2').translate(0, 150);
+
+var l1 = new joint.shapes.standard.Link({
     source: { id: rb.id },
-    target: { id: cb.id },
+    target: { id: cb1.id },
     router: { name: 'manhattan' },
     attrs: {
-        '.marker-target': {
-            d: 'M 10 0 L 0 5 L 10 10 z',
-            filter: 'url(#sketched-filter)'
-        },
-        '.connection': {
+        line: {
             filter: 'url(#sketched-filter)',
-            'stroke-width': 3
+            strokeWidth: 3
         }
     }
 });
-graph.addCell(l1);
 
-var cb2 = cb.clone().attr('text/text', 'My Circle 2').addTo(graph);
-cb2.translate(0, 150);
-var l2 = l1.clone().set({ source: { id: rb.id }, target: { id: cb2.id } }).addTo(graph);
+var l2 = l1.clone().set({
+    source: { id: rb.id },
+    target: { id: cb2.id }
+});
 
-var sketchedFilter = V('<filter filterUnits="userSpaceOnUse" id="sketched-filter" color-interpolation-filters="sRGB"><feTurbulence result="result91" id="feTurbulence3810" type="turbulence" stitchTiles="noStitch" numOctaves="3" baseFrequency="0.04" seed="0"></feTurbulence><feDisplacementMap scale="6.6" in="SourceGraphic" in2="result91" id="feDisplacementMap3812" xChannelSelector="R" yChannelSelector="G"></feDisplacementMap></filter>');
-V(paper.svg).defs().append(sketchedFilter);
+graph.addCells([rb, cb1, cb2, l1, l2]);
+
+var filterXML = '<filter filterUnits="userSpaceOnUse" id="sketched-filter" color-interpolation-filters="sRGB"><feTurbulence result="result91" id="feTurbulence3810" type="turbulence" stitchTiles="noStitch" numOctaves="3" baseFrequency="0.04" seed="0"></feTurbulence><feDisplacementMap scale="6.6" in="SourceGraphic" in2="result91" id="feDisplacementMap3812" xChannelSelector="R" yChannelSelector="G"></feDisplacementMap></filter>';
+V(filterXML).appendTo(paper.defs);
