6cc9ee1d083140a26cdd22aabd1d1e5cf8ae0f96
Roman Bruckner <bruckner.roman@gmail.com>
2018-10-20 21:15:31 +0200

    update

--- demo/nested-clone.js
+++ demo/nested-clone.js
@@ -1,6 +1,4 @@
-var $info = $('<pre>');
-$info.css({ position: 'fixed', top: 50, right: 100 });
-$(document.body).append($info);
+var $info = $('<pre>').css({ position: 'fixed', top: 50, right: 100 }).appendTo(document.body);
 resetInfo();
 
 function resetInfo() {
@@ -8,75 +6,84 @@ function resetInfo() {
 }
 
 function me(id, x, y, w, h, fill) {
-    return new joint.shapes.basic.Rect({
+    return new joint.shapes.standard.Rectangle({
         id: id,
         name: id,
         position: { x: x, y: y },
         size: { width: w, height: h },
         attrs: {
-            rect: { fill: fill || 'blue' },
-            text: { text: id, fill: 'white', 'ref-x': 10, 'ref-y': 10, 'text-anchor': 'start' }
+            body: {
+                fill: fill || 'blue'
+            },
+            label: {
+                text: id,
+                fill: 'white',
+                refX: 10,
+                refY: 10,
+                textAnchor: 'start'
+            }
         }
     }).addTo(graph);
 }
 
 function ml(id, s, t, vertices) {
-    return new joint.dia.Link({
+    return new joint.shapes.standard.Link({
         id: id,
         name: id,
         source: { id: s },
         target: { id: t },
         vertices: vertices,
-        labels: [{ position: .5, attrs: { text: { text: id } } }],
-        attrs: {
-            '.marker-target': { d: 'M 6 0 L 0 3 L 6 6 z' }
-        }
+        labels: [{
+            position: .5,
+            attrs: { text: { text: id }}
+        }]
     }).addTo(graph);
 }
 
 // First example.
 
 var graph = new joint.dia.Graph;
-var paper = new joint.dia.Paper({ el: $('#paper'), width: 1000, height: 600, model: graph });
+var paper = new joint.dia.Paper({
+    el: document.getElementById('paper'),
+    width: 1000,
+    height: 600,
+    model: graph
+});
 
 var a = me('a', 100, 30, 420, 200, 'lightblue');
 var aa = me('aa', 130, 50, 160, 140, 'green');
-a.embed(aa);
 var aaa = me('aaa', 150, 120, 120, 40, 'gray');
+a.embed(aa);
 aa.embed(aaa);
-var c = me('c', 400, 50, 50, 50, 'orange');
-a.embed(c);
-var d = me('d', 620, 50, 50, 50, 'black');
+a.embed(me('c', 400, 50, 50, 50, 'orange'));
+me('d', 620, 50, 50, 50, 'black');
+ml('l1', 'aa', 'c');
+aa.embed(ml('l2', 'aa', 'aaa', [{ x: 50, y: 110 }, { x: 50, y: 140 }]));
+ml('l3', 'c', 'd');
 
-var l1 = ml('l1', 'aa', 'c');
-var l2 = ml('l2', 'aa', 'aaa', [{ x: 50, y: 110 }, { x: 50, y: 140 }]);
-aa.embed(l2);
-var l3 = ml('l3', 'c', 'd');
-//var l4 = ml('l4', 'a', 'c');
+paper.on('cell:mouseleave', resetInfo);
 
-paper.on('cell:mouseout', resetInfo);
-
-paper.on('cell:mouseover', function(cellView) {
+paper.on('cell:mouseenter', function(cellView) {
     var cell = cellView.model;
     var i = {};
     var keyCloneCells = 'graph.cloneCells([' + cell.id + '], { deep: true })';
-    i[keyCloneCells] = _.map(graph.cloneCells([cell], { deep: true }), function(c) {
+    i[keyCloneCells] = joint.util.toArray(graph.cloneCells([cell], { deep: true })).map(function(c) {
         return c.get('name');
     }).join(',');
     var keyCloneSubgraph = 'graph.cloneSubgraph([' + cell.id + '], { deep: true })';
-    i[keyCloneSubgraph] = _.map(graph.cloneSubgraph([cell], { deep: true }), function(c) {
+    i[keyCloneSubgraph] = joint.util.toArray(graph.cloneSubgraph([cell], { deep: true })).map(function(c) {
         return c.get('name');
     }).join(',');
     var keyClone = cell.id + '.clone({ deep: true })';
-    i[keyClone] = _.map(cell.clone({ deep: true }), function(c) {
+    i[keyClone] = joint.util.toArray(cell.clone({ deep: true })).map(function(c) {
         return c.get('name');
     }).join(',');
     var keySubgraph = 'graph.getSubgraph([' + cell.id + '], { deep: true })';
-    i[keySubgraph] = _.map(graph.getSubgraph([cell], { deep: true }), function(c) {
+    i[keySubgraph] = joint.util.toArray(graph.getSubgraph([cell], { deep: true })).map(function(c) {
         return c.get('name');
     }).join(',');
     var keyGetConnectedLinks = 'graph.getConnectedLinks(' + cell.id + ', { deep: true })';
-    i[keyGetConnectedLinks] = _.map(graph.getConnectedLinks(cell, { deep: true }), function(c) {
+    i[keyGetConnectedLinks] = joint.util.toArray(graph.getConnectedLinks(cell, { deep: true })).map(function(c) {
         return c.get('name');
     }).join(',');
     $info.text(JSON.stringify(i, '\t', 4));
--- demo/nested.js
+++ demo/nested.js
@@ -1,47 +1,36 @@
 var graph = new joint.dia.Graph;
 
-var paper = new joint.dia.Paper({
-
-    el: $('#paper'),
+new joint.dia.Paper({
+    el: document.getElementById('paper'),
     width: 800,
     height: 600,
     gridSize: 10,
     model: graph
 });
 
-var r1 = new joint.shapes.basic.Rect({
+var r1 = new joint.shapes.standard.Rectangle({
     position: { x: 20, y: 20 },
     size: { width: 700, height: 300 },
     attrs: {
-        rect: { fill: 'orange' },
-        text: { text: 'Box' }
+        body: { fill: 'orange' },
+        label: { text: 'Box' }
     }
 }).addTo(graph);
 
 var r11 = r1.clone();
-r11.resize(300, 150).attr({ rect: { fill: 'yellow' } }).translate(10, 10).addTo(graph);
+r11.resize(300, 150).attr({ body: { fill: 'yellow' }}).translate(10, 10).addTo(graph);
 
 var r12 = r11.clone();
-r12.resize(200, 150).attr({ rect: { fill: 'yellow' } }).translate(400).addTo(graph);
+r12.resize(200, 150).attr({ body: { fill: 'yellow' }}).translate(400).addTo(graph);
 
 var l1 = new joint.dia.Link({
     source: { id: r11.id },
-    target: { id: r12.id },
-    attrs: {
-        '.marker-target': {
-            d: 'M 10 0 L 0 5 L 10 10 z'
-        }
-    }
+    target: { id: r12.id }
 }).addTo(graph);
 
-var l2 = new joint.dia.Link({
+var l2 = new joint.shapes.standard.Link({
     source: { x: 100, y: 100 },
-    target: { x: 200, y: 200 },
-    attrs: {
-        '.marker-target': {
-            d: 'M 10 0 L 0 5 L 10 10 z'
-        }
-    }
+    target: { x: 200, y: 200 }
 }).addTo(graph);
 
 r1.embed(r11);
@@ -49,7 +38,9 @@ r1.embed(r12);
 r1.embed(l1);
 r1.embed(l2);
 
-var r1clones = r1.clone({ deep: true })
+var r1clones = r1.clone({ deep: true });
 
-_.each(r1clones, function(clone) { graph.addCell(clone); });
+Object.keys(r1clones).forEach(function(id) {
+    graph.addCell(r1clones[id]);
+});
 
