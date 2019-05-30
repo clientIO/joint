2323527324a378a190bf8b90256a100279ddd7c0
vladimir talas <lades@client.io>
2019-05-30 23:33:14 +0200

    gruntfile typo

--- grunt/config/concat.js
+++ grunt/config/concat.js
@@ -31,7 +31,7 @@ module.exports = function(grunt) {
                     ),
                 'build/joint.css':
                     [].concat(
-                        core.css,
+                        core.css
                     )
             }
         }
