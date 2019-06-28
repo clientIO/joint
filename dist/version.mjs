83244f84f1dbeb77fb69866847a3ff98371689a0
Roman Bruckner <bruckner.roman@gmail.com>
2019-06-28 13:29:43 +0200

    update

--- dist/version.mjs
+++ dist/version.mjs
@@ -1,10 +1,3 @@
-/*! JointJS v3.0.2 (2019-06-27) - JavaScript diagramming library
-
-
-This Source Code Form is subject to the terms of the Mozilla Public
-License, v. 2.0. If a copy of the MPL was not distributed with this
-file, You can obtain one at http://mozilla.org/MPL/2.0/.
-*/
 var version = "3.0.2";
 
 export { version };
