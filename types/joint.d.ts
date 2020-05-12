3e0d3d0939a93f5f86bd2804c5ac905fad28aa72
Roman Bruckner <bruckner.roman@gmail.com>
2020-05-12 17:11:24 +0200

    util: fix debounce() TS definition

--- types/joint.d.ts
+++ types/joint.d.ts
@@ -2426,6 +2426,11 @@ export namespace util {
     type IterateeFunction = (value: any) => NotVoid;
     type IterateeShorthand = PropertyPath; // there are other shorthands in Lodash but not in the methods we duplicate
 
+    interface Cancelable {
+        cancel(): void;
+        flush(): void;
+    }
+
     type SourceObjectsOptionalFinalCustomizer = Array<object | CustomizerFunction>; // typescript cannot express "any number of objects optionally followed by CustomizerFunction"
     type CustomizerFunction = (objValue: any, srcValue: any, key: string, object: any, source: any, stack: any) => NotVoid;
 
@@ -2469,7 +2474,7 @@ export namespace util {
 
     export function toArray(value: any): any[];
 
-    export function debounce(func: Function, wait?: number, options?: object): Function;
+    export function debounce<T extends Function>(func: T, wait?: number, options?: object): T & Cancelable;
 
     export function groupBy(collection: Collection, iteratee?: Iteratee): object;
 
