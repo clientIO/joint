b49b85b61aabe25444f4aeaa5cbe86ce94f57f67
Roman Bruckner <bruckner.roman@gmail.com>
2020-06-04 19:58:26 +0200

    demo.HTML: add new demo (#1316)

---
+++ demo/html/src/html.js
@@ -0,0 +1,114 @@
+(function(joint, V) {
+
+    // Notes:
+    // - Currently, there is no support for z-indexes on HTML Elements
+    // - It's not possible to export the diagram into PNG/SVG on the client-side
+    // - Do not use CSS background on the root HTML element when using ports
+
+    var graph = new joint.dia.Graph;
+    var paper = new joint.dia.Paper({
+        el: document.getElementById('paper'),
+        width: 850,
+        height: 600,
+        model: graph,
+        async: true,
+        frozen: true,
+        sorting: joint.dia.Paper.sorting.NONE
+    });
+
+    // Container for all HTML views inside paper
+    var htmlContainer = document.createElement('div');
+    htmlContainer.style.pointerEvents = 'none';
+    htmlContainer.style.position = 'absolute';
+    htmlContainer.style.inset = '0';
+    paper.el.appendChild(htmlContainer);
+    paper.htmlContainer = htmlContainer;
+
+    paper.on('scale translate', function() {
+        // Update the transformation of all JointJS HTML Elements
+        var htmlContainer = this.htmlContainer;
+        htmlContainer.style.transformOrigin = '0 0';
+        htmlContainer.style.transform = V.matrixToTransformString(this.matrix());
+    });
+
+    paper.on('blank:pointerdown cell:pointerdown', function() {
+        document.activeElement.blur();
+    });
+
+
+    var el1 = new joint.shapes.html.Element({
+        position: { x: 16, y: 150 },
+        fields: {
+            name: 'Create Story',
+            resource: 'bob',
+            state: 'done'
+        }
+    });
+
+    var el2 = new joint.shapes.html.Element({
+        position: { x: 298, y: 150 },
+        fields: {
+            name: 'Promote',
+            resource: 'mary'
+        }
+    });
+
+    var el3 = new joint.shapes.html.Element({
+        position: { x: 580, y: 150 },
+        fields: {
+            name: 'Measure',
+            resource: 'john',
+            state: 'at-risk'
+        }
+    });
+
+    var l1 = new joint.shapes.standard.Link({
+        source: { id: el1.id },
+        target: { id: el2.id },
+        attrs: {
+            line: {
+                stroke: '#464554'
+            }
+        }
+    });
+
+    var l2 = new joint.shapes.standard.Link({
+        source: { id: el2.id },
+        target: { id: el3.id },
+        attrs: {
+            line: {
+                stroke: '#464554'
+            }
+        }
+    });
+
+    graph.resetCells([el1, el2, el3, l1, l2]);
+
+    paper.unfreeze();
+
+    // Toolbar
+    var zoomLevel = 1;
+
+    document.getElementById('zoom-in').addEventListener('click', function() {
+        zoomLevel = Math.min(3, zoomLevel + 0.2);
+        var size = paper.getComputedSize();
+        paper.translate(0,0);
+        paper.scale(zoomLevel, zoomLevel, size.width / 2, size.height / 2);
+    });
+
+    document.getElementById('zoom-out').addEventListener('click', function() {
+        zoomLevel = Math.max(0.2, zoomLevel - 0.2);
+        var size = paper.getComputedSize();
+        paper.translate(0,0);
+        paper.scale(zoomLevel, zoomLevel, size.width / 2, size.height / 2);
+    });
+
+    document.getElementById('reset').addEventListener('click', function() {
+        graph.getElements().forEach(function(element) {
+            element.prop(['fields', 'name'], '');
+            element.prop(['fields', 'resource'], '');
+            element.prop(['fields', 'state'], '');
+        });
+    });
+
+})(joint, V);
---
+++ demo/html/src/joint.shapes.html.js
@@ -0,0 +1,210 @@
+(function(joint, util, V) {
+
+    var Element = joint.dia.Element;
+    var ElementView = joint.dia.ElementView;
+
+    Element.define('html.Element', {
+        size: { width: 250, height: 228 },
+        fields: {
+            header: 'Task',
+            name: '',
+            resource: '',
+            state: ''
+        },
+        attrs: {
+            placeholder: {
+                refWidth: '100%',
+                refHeight: '100%',
+                fill: 'transparent',
+                stroke: '#D4D4D4'
+            }
+        }
+    }, {
+        markup: [{
+            tagName: 'rect',
+            selector: 'placeholder'
+        }],
+        htmlMarkup: [{
+            tagName: 'div',
+            className: 'html-element',
+            selector: 'htmlRoot',
+            groupSelector: 'field',
+            style: {
+                'position': 'absolute',
+                'pointer-events': 'none',
+                'user-select': 'none',
+                'box-sizing': 'border-box'
+            },
+            attributes: {
+                'data-attribute': 'state'
+            },
+            children: [{
+                tagName: 'label',
+                className: 'html-element-header',
+                groupSelector: 'field',
+                attributes: {
+                    'data-attribute': 'header'
+                }
+            }, {
+                tagName: 'label',
+                className: 'html-element-label',
+                textContent: 'Name',
+                children: [{
+                    tagName: 'input',
+                    className: 'html-element-field',
+                    groupSelector: 'field',
+                    attributes: {
+                        'data-attribute': 'name',
+                        'placeholder': 'Name'
+                    },
+                    style: {
+                        'pointer-events': 'auto'
+                    }
+                }]
+            }, {
+                tagName: 'label',
+                className: 'html-element-label',
+                textContent: 'Resource',
+                children: [{
+                    tagName: 'select',
+                    className: 'html-element-field',
+                    groupSelector: 'field',
+                    attributes: {
+                        'data-attribute': 'resource'
+                    },
+                    style: {
+                        'pointer-events': 'auto'
+                    },
+                    children: [{
+                        tagName: 'option',
+                        textContent: 'Resource',
+                        attributes: {
+                            'value': '',
+                            'disabled': 'true'
+                        }
+                    }, {
+                        tagName: 'option',
+                        textContent: 'John',
+                        attributes: {
+                            'value': 'john'
+                        }
+                    }, {
+                        tagName: 'option',
+                        textContent: 'Mary',
+                        attributes: {
+                            'value': 'mary'
+                        }
+                    }, {
+                        tagName: 'option',
+                        textContent: 'Bob',
+                        attributes: {
+                            'value': 'bob'
+                        }
+                    }]
+                }]
+            }]
+        }]
+    });
+
+    // Custom view for JointJS HTML element that displays an HTML <div></div> above the SVG Element.
+    joint.shapes.html.ElementView = ElementView.extend({
+
+        html: null,
+
+        presentationAttributes: ElementView.addPresentationAttributes({
+            position: ['HTML_UPDATE'],
+            size: ['HTML_UPDATE'],
+            fields: ['HTML_FIELD_UPDATE']
+        }),
+
+        // Run these upon first render
+        initFlag: ElementView.prototype.initFlag.concat([
+            'HTML_UPDATE',
+            'HTML_FIELD_UPDATE'
+        ]),
+
+        confirmUpdate: function() {
+            var flags = ElementView.prototype.confirmUpdate.apply(this, arguments);
+            if (this.hasFlag(flags, 'HTML_UPDATE')) {
+                this.updateHTML();
+                flags = this.removeFlag(flags, 'HTML_UPDATE');
+            }
+            if (this.hasFlag(flags, 'HTML_FIELD_UPDATE')) {
+                this.updateFields();
+                flags = this.removeFlag(flags, 'HTML_FIELD_UPDATE');
+            }
+            return flags;
+        },
+
+        onRender: function() {
+            this.removeHTMLMarkup();
+            this.renderHTMLMarkup();
+            return this;
+        },
+
+        renderHTMLMarkup: function() {
+            var doc = util.parseDOMJSON(this.model.htmlMarkup, V.namespace.xhtml);
+            var html = doc.selectors.htmlRoot;
+            var fields = doc.groupSelectors.field;
+            // React on all box changes. e.g. input change
+            html.addEventListener('change', this.onFieldChange.bind(this), false);
+            this.paper.htmlContainer.appendChild(doc.fragment);
+            this.html = html;
+            this.fields = fields;
+        },
+
+        removeHTMLMarkup: function() {
+            var html = this.html;
+            if (!html) return;
+            this.paper.htmlContainer.removeChild(html);
+            this.html = null;
+            this.fields = null;
+        },
+
+        updateHTML: function() {
+            var bbox = this.model.getBBox();
+            var html = this.html;
+            html.style.width = bbox.width + 'px';
+            html.style.height = bbox.height + 'px';
+            html.style.left = bbox.x + 'px';
+            html.style.top = bbox.y + 'px';
+        },
+
+        onFieldChange: function(evt) {
+            var input = evt.target;
+            var attribute = input.dataset.attribute;
+            if (attribute) {
+                this.model.prop(['fields', attribute], input.value);
+            }
+        },
+
+        updateFields: function() {
+            this.fields.forEach(function(field) {
+                var attribute = field.dataset.attribute;
+                var value = this.model.prop(['fields', attribute]);
+                switch (field.tagName.toUpperCase()) {
+                    case 'LABEL':
+                        field.textContent = value;
+                        break;
+                    case 'INPUT':
+                    case 'SELECT':
+                        field.value = value;
+                        if (value) {
+                            field.classList.remove('field-empty');
+                        } else {
+                            field.classList.add('field-empty');
+                        }
+                        break;
+                    case 'DIV':
+                        field.dataset[attribute] = value;
+                        break;
+                }
+            }.bind(this));
+        },
+
+        onRemove: function() {
+            this.removeHTMLMarkup();
+        }
+    });
+
+})(joint, joint.util, V);
