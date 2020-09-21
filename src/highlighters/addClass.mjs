318d1b9bb2d22e7d5ad84fc98e9e5d0386680118
Roman Bruckner <bruckner.roman@gmail.com>
2020-09-21 17:29:12 +0200

    update

--- src/highlighters/addClass.mjs
+++ src/highlighters/addClass.mjs
@@ -2,12 +2,14 @@ import * as util from '../util/index.mjs';
 import V from '../V/index.mjs';
 import { HighlighterView } from '../dia/HighlighterView.mjs';
 
+const className = util.addClassNamePrefix('highlighted');
+
 export const addClass = HighlighterView.extend({
 
     UPDATABLE: false,
 
     options: {
-        className: util.addClassNamePrefix('highlighted')
+        className
     },
 
     highlight: function(_cellView, node) {
@@ -18,4 +20,7 @@ export const addClass = HighlighterView.extend({
         V(node).removeClass(this.options.className);
     }
 
+}, {
+    // Backwards Compatibility
+    className
 });
