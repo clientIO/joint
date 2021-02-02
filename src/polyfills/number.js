5ca45e623c6f7211bf7ed58ab81dda385f2d46d5
vladimir talas <vladimir.talas@gmail.com>
2021-02-02 10:21:08 +0100

    remove commnets

--- src/polyfills/number.js
+++ src/polyfills/number.js
@@ -1,14 +1,3 @@
 import 'core-js/stable/number/is-nan';
 import 'core-js/stable/number/is-finite';
 
-/*
-Number.isFinite = Number.isFinite || function(value) {
-    return typeof value === 'number' && isFinite(value);
-};
-
-//The following works because NaN is the only value in javascript which is not equal to itself.
-Number.isNaN = Number.isNaN || function(value) {
-    return value !== value;
-};
-
-*/
--- src/polyfills/string.js
+++ src/polyfills/string.js
@@ -1,23 +1,3 @@
 import 'core-js/stable/string/includes';
 import 'core-js/stable/string/starts-with';
 
-// if (!String.prototype.includes) {
-//     String.prototype.includes = function(search, start) {
-//         'use strict';
-//         if (typeof start !== 'number') {
-//             start = 0;
-//         }
-//
-//         if (start + search.length > this.length) {
-//             return false;
-//         } else {
-//             return this.indexOf(search, start) !== -1;
-//         }
-//     };
-// }
-//
-// if (!String.prototype.startsWith) {
-//     String.prototype.startsWith = function(searchString, position){
-//         return this.substr(position || 0, searchString.length) === searchString;
-//     };
-// }
