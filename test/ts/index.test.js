e30a47c8ff05997a15284af7ee17e972c59809f8
Roman Bruckner <bruckner.roman@gmail.com>
2021-09-13 12:33:06 +0200

    update

--- test/ts/index.test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var joint = require("../../build/joint");
-var graph = new joint.dia.Graph;
-graph.addCells([]);
-//# sourceMappingURL=index.test.js.map
