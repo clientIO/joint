197a3050e274913feccbb3b56801a6569ef6c3a5
Roman Bruckner <bruckner.roman@gmail.com>
2021-09-27 10:58:54 +0200

    dia.Graph: prevent attribute type inference from the constructor

--- types/joint.d.ts
+++ types/joint.d.ts
@@ -94,14 +94,14 @@ export namespace dia {
         }
 
         interface Attributes {
-            cells: Cells,
+            cells?: Cells;
             [key: string]: any;
         }
     }
 
     class Graph<A = Graph.Attributes, S = dia.ModelSetOptions> extends Backbone.Model<A, S> {
 
-        constructor(attributes?: A, opt?: { cellNamespace?: any, cellModel?: typeof Cell });
+        constructor(attributes?: Graph.Attributes, opt?: { cellNamespace?: any, cellModel?: typeof Cell });
 
         addCell(cell: Cell | Cell[], opt?: CollectionAddOptions): this;
 
