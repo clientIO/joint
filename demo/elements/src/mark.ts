9b02f22f9efca5b50594dff54cde9c36fd81a812
Roman Bruckner <bruckner.roman@gmail.com>
2021-10-04 18:59:27 +0200

    update

---
+++ demo/elements/src/mark.ts
@@ -0,0 +1,43 @@
+import { dia } from 'jointjs';
+
+export default class Mark extends dia.Element {
+
+    static version = '3.4';
+
+    defaults() {
+        return {
+            ...super.defaults,
+            type: 'Mark',
+            size: {
+                width: 120,
+                height: 50
+            },
+            attrs: {
+                body: {
+                    strokeWidth: 2,
+                    stroke: '#333333',
+                    fill: '#FFF',
+                    d: 'M 0 calc(0.5*h) calc(0.5*h) 0 H calc(w-calc(0.5 * h)) a 3 3 0 0 1 3 calc(h) H calc(0.5*h) z'
+                },
+                label: {
+                    text: 'Mark',
+                    textVerticalAnchor: 'middle',
+                    textAnchor: 'middle',
+                    y: 'calc(0.5*h)',
+                    x: 'calc(0.5*w)',
+                    fontSize: 13,
+                    fontFamily: 'sans-serif',
+                    fill: '#333333'
+                }
+            }
+        }
+    }
+
+    markup = [{
+        tagName: 'path',
+        selector: 'body'
+    }, {
+        tagName: 'text',
+        selector: 'label'
+    }];
+}
