4cf0f3e1a866267e46764490d0f82251eda79962
Roman Bruckner <bruckner.roman@gmail.com>
2021-10-29 16:02:21 +0200

    linkTools: add handleClass option types for Vertices and Segments (#1556)

---
+++ test/ts/toolsView.test.ts
@@ -0,0 +1,27 @@
+import { dia, linkTools } from '../../build/joint';
+
+class MyVertexHandle extends linkTools.Vertices.VertexHandle {
+}
+
+const vertices = new linkTools.Vertices({
+    handleClass: MyVertexHandle,
+    vertexAdding: false,
+});
+
+class MySegmentHandle extends linkTools.Segments.SegmentHandle {
+}
+
+const segments = new linkTools.Segments({
+    handleClass: MySegmentHandle,
+    snapHandle: false,
+});
+
+const toolsView = new dia.ToolsView({
+    name: 'test-tools-view',
+    tools: [
+        vertices,
+        segments,
+    ]
+});
+
+toolsView.configure({ component: false });
