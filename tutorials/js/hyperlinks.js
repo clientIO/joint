ee1fbecc6161152c60b0c58fa46a888fc84584ab
James Williams <jamesgeorgewilliams@protonmail.com>
2021-11-25 09:40:03 +0100

    tutorials: update custom links and elements with calc

--- tutorials/js/hyperlinks.js
+++ tutorials/js/hyperlinks.js
@@ -28,8 +28,8 @@
                 stroke: '#000000'
             },
             link: {
-                refWidth: '100%',
-                refHeight: '100%',
+                width: 'calc(w)',
+                height: 'calc(h)',
                 xlinkShow: 'new',
                 cursor: 'pointer'
             },
