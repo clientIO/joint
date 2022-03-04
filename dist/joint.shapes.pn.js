7ee6078dd3051827bd867972f0b7cea517a34780
Roman Bruckner <bruckner.roman@gmail.com>
2022-03-04 14:17:20 +0100

    Release v3.5.4 (#1631)

--- dist/joint.shapes.pn.js
+++ dist/joint.shapes.pn.js
@@ -1,4 +1,4 @@
-/*! JointJS v3.5.3 (2022-02-21) - JavaScript diagramming library
+/*! JointJS v3.5.4 (2022-03-04) - JavaScript diagramming library
 
 
 This Source Code Form is subject to the terms of the Mozilla Public
