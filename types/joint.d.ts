c7e83f6cb99caf68d4b05b3cafca5a4b11039c16
Arthur Khokhlov <arthur.khokhlov@gmail.com>
2022-07-13 17:32:44 +0200

    type

--- types/joint.d.ts
+++ types/joint.d.ts
@@ -2786,6 +2786,8 @@ export namespace util {
 
     export function uuid(): string;
 
+    export function svg(obj: TemplateStringsArray): dia.MarkupJSON;
+
     export function guid(obj?: { [key: string]: any }): string;
 
     export function toKebabCase(str: string): string;
