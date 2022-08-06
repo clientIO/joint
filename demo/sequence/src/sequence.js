8a6604e8498a7fafbe8a9b9de8368c97f32949f1
Roman Bruckner <bruckner.roman@gmail.com>
2022-08-06 17:03:48 +0200

    update demo sequence

--- demo/sequence/src/sequence.js
+++ demo/sequence/src/sequence.js
@@ -1,10 +1,11 @@
-(function(dia, sd, linkTools, connectionStrategies, paperElement) {
+(function(dia, sd, linkTools, elementTools, connectionStrategies, highlighters, paperElement) {
 
-    var paperWidth = 800;
-    var paperHeight = 600;
+    const paperWidth = 800;
+    const paperHeight = 600;
+    const topY = 20;
 
-    var graph = new dia.Graph();
-    var paper = new dia.Paper({
+    const graph = new dia.Graph();
+    const paper = new dia.Paper({
         el: paperElement,
         width: paperWidth,
         height: paperHeight,
@@ -17,9 +18,10 @@
         moveThreshold: 5,
         linkPinning: false,
         markAvailable: true,
+        preventDefaultBlankAction: false,
         restrictTranslate: function(elementView) {
-            var element = elementView.model;
-            var padding = (element.isEmbedded()) ? 20 : 10;
+            const element = elementView.model;
+            const padding = (element.isEmbedded()) ? 20 : 10;
             return {
                 x: padding,
                 y: element.getBBox().y,
@@ -28,12 +30,12 @@
             };
         },
         interactive: function(cellView) {
-            var cell = cellView.model;
+            const cell = cellView.model;
             return (cell.isLink())
                 ? { linkMove: false, labelMove: false }
                 : true;
         },
-        defaultLink: (sourceView) => {
+        defaultLink: function(sourceView) {
             const type = sourceView.model.get('type');
             switch (type) {
                 case 'sd.Message': {
@@ -97,12 +99,12 @@
         const type = linkView.model.get('type');
         switch (type) {
             case 'sd.Message': {
-                var sView = linkView.sourceView;
-                var tView = linkView.targetView;
+                const sView = linkView.sourceView;
+                const tView = linkView.targetView;
                 if (!sView || !tView) return;
-                var padding = 20;
-                var minY = Math.max(tView.sourcePoint.y - sView.sourcePoint.y, 0) + padding;
-                var maxY = sView.targetPoint.y - sView.sourcePoint.y - padding;
+                const padding = 20;
+                const minY = Math.max(tView.sourcePoint.y - sView.sourcePoint.y, 0) + padding;
+                const maxY = sView.targetPoint.y - sView.sourcePoint.y - padding;
                 linkView.model.setStart(Math.min(Math.max(y - sView.sourcePoint.y, minY), maxY));
                 break;
             }
@@ -116,118 +118,255 @@
         const type = linkView.model.get('type');
         switch (type) {
             case 'sd.Message': {
-                linkView.model.setDescription('Message');
+                editText(linkView, 'labels/0/attrs/labelText/text');
                 break;
             }
             case 'sd.LifeSpan': {
                 break;
             }
         }
-
     });
 
-    var role1 = new sd.Role({ position: { x: 100, y: 20 }});
+    const toolsScale = 1.2;
+
+    graph.on('add', function(link) {
+        if (!link.isLink()) return;
+        const type = link.get('type');
+        switch (type) {
+            case 'sd.Lifeline': {
+                const tools = new dia.ToolsView({
+                    layer: null,
+                    tools: [
+                        new linkTools.HoverConnect({
+                            scale: toolsScale
+                        }),
+                    ]
+                });
+                link.findView(paper).addTools(tools);
+                break;
+            }
+        }
+    });
+
+    paper.on('cell:mouseenter', function(cellView) {
+        const cell = cellView.model;
+        const type = cell.get('type');
+        switch (type) {
+            case 'sd.Message': {
+                const tools = new dia.ToolsView({
+                    tools: [
+                        new linkTools.Connect({
+                            scale: toolsScale,
+                            distance: -20
+                        }),
+                        new linkTools.Remove({
+                            scale: toolsScale,
+                            distance: 15
+                        })
+                    ]
+                });
+                cellView.addTools(tools);
+                break;
+            }
+            case 'sd.LifeSpan': {
+                const tools = new dia.ToolsView({
+                    tools: [
+                        new linkTools.Remove({
+                            scale: toolsScale,
+                            distance: 15
+                        })
+                    ]
+                });
+                cellView.addTools(tools);
+                break;
+            }
+            case 'sd.Role': {
+                const tools = new dia.ToolsView({
+                    tools: [
+                        new elementTools.Remove({
+                            scale: toolsScale,
+                            distance: '50%'
+                        })
+                    ]
+                });
+                cellView.addTools(tools);
+                break;
+            }
+        }
+    });
+
+    paper.on('cell:mouseleave', function(cellView) {
+        const cell = cellView.model;
+        const type = cell.get('type');
+        switch (type) {
+            case 'sd.Role':
+            case 'sd.LifeSpan':
+            case 'sd.Message': {
+                cellView.removeTools();
+                break;
+            }
+        }
+    });
+
+    paper.on('blank:pointerdblclick', function(evt, x, y) {
+        const role = new sd.Role({ position: { x: x - 50, y: topY }});
+        role.addTo(graph);
+        const lifeline = new sd.Lifeline();
+        lifeline.attachToRole(role, paperHeight);
+        lifeline.addTo(graph);
+        editText(role.findView(paper), 'attrs/label/text');
+    });
+
+    const role1 = new sd.Role({ position: { x: 100, y: topY }});
     role1.setName('Browser');
     role1.addTo(graph);
 
-    var role2 = new sd.Role({ position: { x: 400, y: 20 }});
+    const role2 = new sd.Role({ position: { x: 400, y: topY }});
     role2.setName('Web Server');
     role2.addTo(graph);
 
-    var role3 = new sd.Role({ position: { x: 600, y: 20 }});
+    const role3 = new sd.Role({ position: { x: 600, y: topY }});
     role3.setName('Database Server');
     role3.addTo(graph);
 
-    var backend = new sd.RoleGroup();
+    const backend = new sd.RoleGroup();
     backend.embed(role2);
     backend.embed(role3);
     backend.addTo(graph);
     backend.fitRoles();
     backend.listenTo(graph, 'change:position', function(cell) {
-        if (cell.isEmbeddedIn(this)) this.fitRoles();
+        if (cell.isEmbeddedIn(backend)) backend.fitRoles();
+    });
+    graph.on('remove', function(element) {
+        if (!element.isElement()) return;
+        const embeds = backend.getEmbeddedCells();
+        if (embeds.length < 2) {
+            backend.unembed(embeds);
+            backend.remove();
+        }
     });
 
-    var lifeline1 = new sd.Lifeline();
+    const lifeline1 = new sd.Lifeline();
     lifeline1.attachToRole(role1, paperHeight);
     lifeline1.addTo(graph);
 
-    var lifeline2 = new sd.Lifeline();
+    const lifeline2 = new sd.Lifeline();
     lifeline2.attachToRole(role2, paperHeight);
     lifeline2.addTo(graph);
 
-    var lifeline3 = new sd.Lifeline();
+    const lifeline3 = new sd.Lifeline();
     lifeline3.attachToRole(role3, paperHeight);
     lifeline3.addTo(graph);
 
-    var message1 = new sd.Message();
+    const message1 = new sd.Message();
     message1.setFromTo(lifeline1, lifeline2);
     message1.setStart(50);
     message1.setDescription('HTTP GET Request');
     message1.addTo(graph);
 
-    var message2 = new sd.Message();
+    const message2 = new sd.Message();
     message2.setFromTo(lifeline2, lifeline3);
     message2.setStart(150);
     message2.setDescription('SQL Command');
     message2.addTo(graph);
 
-    var message3 = new sd.Message();
+    const message3 = new sd.Message();
     message3.setFromTo(lifeline3, lifeline2);
     message3.setStart(250);
     message3.setDescription('Result Set');
     message3.addTo(graph);
 
-    var message4 = new sd.Message();
+    const message4 = new sd.Message();
     message4.setFromTo(lifeline2, lifeline1);
     message4.setStart(350);
     message4.setDescription('HTTP Response');
     message4.addTo(graph);
 
-    var lifespan1 = new sd.LifeSpan();
+    const lifespan1 = new sd.LifeSpan();
     lifespan1.attachToMessages(message2, message3);
     lifespan1.addTo(graph);
 
-    graph.getLinks().forEach(function(link) {
-        if (link instanceof sd.Lifeline) {
-            const tools = new dia.ToolsView({
-                tools: [
-                    new linkTools.HoverConnect({ scale: 1.5 }),
-                ]
-            });
-            link.findView(paper).addTools(tools);
-        }
-    });
-
-    paper.on('link:mouseenter', function(linkView) {
-        const link = linkView.model;
-        if (link instanceof sd.Message) {
-            const tools = new dia.ToolsView({
-                tools: [
-                    new linkTools.Connect({
-                        distance: -20
-                    }),
-                    new linkTools.Remove({
-                        distance: -40
-                    })
-                ]
-            });
-            linkView.addTools(tools);
-        }
-    });
-
-    paper.on('link:mouseleave', function(linkView) {
-        const link = linkView.model;
-        if (link instanceof sd.Message) {
-            linkView.removeTools();
-        }
-    });
-
     paper.unfreeze();
+
+    // Text Editing
+
+    function editText(cellView, textPath) {
+
+        const cell = cellView.model;
+        const textarea = document.createElement('textarea');
+        textarea.style.position = 'absolute';
+        textarea.style.width ='200px';
+        textarea.style.height = '100px';
+        textarea.style.left = '50%';
+        textarea.style.top = `${paper.options.height / 2}px`;
+        textarea.style.transform = 'translate(-50%, -50%)';
+        textarea.style.padding = '5px';
+        textarea.style.resize = 'none';
+        textarea.style.boxShadow = '10px 10px 5px rgba(0, 0, 0, 0.5)';
+        textarea.placeholder = cell.placeholder || 'Enter text here...';
+        textarea.value = cell.prop(textPath) || '';
+        document.body.appendChild(textarea);
+        textarea.focus();
+        textarea.setSelectionRange(0, textarea.value.length);
+
+        cellView.paper.el.style.filter = 'blur(0.5px) grayscale(1)';
+        cellView.paper.el.style.pointerEvents = 'none';
+
+        const highlighter = highlighters.mask.add(cellView, 'root', 'selection', {
+            layer: dia.Paper.Layers.FRONT,
+            deep: true
+        });
+
+        function close() {
+            textarea.remove();
+            cellView.paper.el.style.filter = '';
+            cellView.paper.el.style.pointerEvents = '';
+            highlighter.remove();
+        }
+
+        function saveText() {
+            cell.prop(textPath, textarea.value);
+            close();
+        }
+
+        textarea.addEventListener('blur', saveText);
+
+        textarea.addEventListener('keydown', function(evt) {
+            if (evt.key === 'Enter' && !evt.shiftKey) {
+                textarea.blur();
+            }
+            if (evt.key === 'Escape') {
+                textarea.removeEventListener('blur', saveText);
+                close();
+            }
+        });
+    }
+
+    paper.on('link:pointerdblclick', function(linkView, evt) {
+        const labelIndex = linkView.findAttribute('label-idx', evt.target);
+        if (!labelIndex) return;
+        editText(linkView, `labels/${labelIndex}/attrs/labelText/text`);
+    });
+
+    paper.on('element:pointerdblclick', function(elementView, evt) {
+        switch (elementView.model.get('type')) {
+            case 'sd.Role': {
+                editText(elementView, 'attrs/label/text');
+                break;
+            }
+            case 'sd.RoleGroup': {
+                editText(elementView, 'attrs/label/text');
+                break;
+            }
+        }
+    });
 
 })(
     joint.dia,
     joint.shapes.sd,
     joint.linkTools,
+    joint.elementTools,
     joint.connectionStrategies,
+    joint.highlighters,
     document.getElementById('paper')
 );
