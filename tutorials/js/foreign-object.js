f16ef254274ab9c0a8a64519d83c83e218442044
James Williams <jamesgeorgewilliams@protonmail.com>
2023-04-25 17:10:18 +0200

    docs(tutorials): add foreignObject tutorial

--- tutorials/js/foreign-object.js
+++ tutorials/js/foreign-object.js
@@ -15,26 +15,12 @@
         background: {
             color: '#F7F7F7'
         },
-        preventDefaultBlankAction: false
-        guard: (evt) => ['SPAN'].includes(evt.target.tagName)
+        // guard: (evt) => ['SPAN'].includes(evt.target.tagName)
     });
 
-    // const paper = new joint.dia.Paper({
-    //     el: document.getElementById('paper-foreign-object-prevent-default'),
-    //     model: graph,
-    //     width: 500,
-    //     height: 350,
-    //     async: true,
-    //     frozen: true,
-    //     cellViewNamespace: namespace,
-    //     background: {
-    //         color: '#F7F7F7'
-    //     },
-    // });
-
-    // paper.on('blank:pointerdown cell:pointerdown', () => {
-    //     document.activeElement.blur();
-    // });
+    paper.on('blank:pointerdown cell:pointerdown', () => {
+        document.activeElement.blur();
+    });
 
     const Form = joint.dia.Element.define('example.Form', {
         attrs: {
@@ -46,14 +32,14 @@
     }, {
         markup: joint.util.svg/* xml */`
             <foreignObject @selector="foreignObject">
-                <div @selector="outer"
+                <div
                     xmlns="http://www.w3.org/1999/xhtml"
                     class="outer"
                 >
-                    <div @selector="inner" class="inner">
-                        <form @selector="form" class="form">
-                            <input @selector="name" type="text" id="name" name="name" autocomplete="off" placeholder="Your diagram name"/>
-                            <button @selector="submit">
+                    <div class="inner">
+                        <form class="form">
+                            <input @selector="name" type="text" name="name" autocomplete="off" placeholder="Your diagram name"/>
+                            <button>
                                 <span>Submit</span>
                             </button>
                         </form>
@@ -71,7 +57,7 @@
 
         onSubmit: function(evt) {
             evt.preventDefault();
-            evt.target.children.name.value = '';
+            // evt.target.children.name.value = '';
             this.model.attr('name/props/value', '');
         },
     
@@ -81,132 +67,6 @@
     form.position(72, 70);
     form.resize(355, 200);
     form.addTo(graph);
-
-    const Card = joint.dia.Element.define('example.ForeignObject', {
-        attrs: {
-            body: {
-                width: 'calc(w)',
-                height: 'calc(h)',
-                fill: {
-                    type: 'linearGradient',
-                    stops: [
-                        { offset: 0, color: '#ff5c69' },
-                        { offset: 0.5, color: '#ff4252' },
-                        { offset: 1, color: '#ed2637' }
-                    ]
-                }
-            },
-            foreignObject: {
-                width: 'calc(w-12)',
-                height: 'calc(h-12)',
-                x: 6,
-                y: 6
-            }
-        },
-    }, {
-        markup: [
-            {
-                tagName: 'rect',
-                selector: 'body'
-            },
-            {
-                tagName: 'foreignObject',
-                selector: 'foreignObject',
-                children: [
-                    {
-                        tagName: 'div',
-                        namespaceURI: 'http://www.w3.org/1999/xhtml',
-                        selector: 'background',
-                        style: {
-                            backgroundColor: '#131e29',
-                            width: '100%',
-                            height: '100%'
-                        },
-                        children: [
-                            {
-                                tagName: 'p',
-                                selector: 'text',
-                                style: {
-                                    color: '#F2F2F2',
-                                    font: '16px sans-serif',
-                                    padding: '10px',
-                                    margin: 0,
-                                },
-                                textContent: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    });
-
-    // const Card = joint.dia.Element.define('example.ForeignObject', {
-    //     attrs: {
-    //         body: {
-    //             width: 'calc(w)',
-    //             height: 'calc(h)',
-    //             // width: 180,
-    //             // height: 100,
-    //             fill: {
-    //                 type: 'linearGradient',
-    //                 stops: [
-    //                     { offset: 0, color: '#ff5c69' },
-    //                     { offset: 0.5, color: '#ff4252' },
-    //                     { offset: 1, color: '#ed2637' }
-    //                 ]
-    //             }
-    //         },
-    //         foreignObject: {
-    //             width: 'calc(w-12)',
-    //             height: 'calc(h-12)',
-    //             x: 6,
-    //             y: 6
-    //         }
-    //     },
-    // }, {
-    //     // The /* xml */ comment is optional.
-    //     // It is used to tell the IDE that the markup is XML.
-    //     markup: joint.util.svg/* xml */`
-    //         <style>
-    //             div.card__background {
-    //                 background-color: #131e29;
-    //                 height: 100%;
-    //             }
-    //             p.card__text {
-    //                 color: #F2F2F2;
-    //                 font: 16px sans-serif;
-    //                 padding: 10px;
-    //                 margin: 0;
-    //             }             
-    //         </style>
-    //         <rect @selector="body"/>
-    //         <foreignObject @selector="foreignObject">
-    //             <div @selector="background"
-    //                 xmlns="http://www.w3.org/1999/xhtml"
-    //                 class="card__background"
-    //             >
-    //                 <p @selector="text"
-    //                     class="card__text"
-    //                 >
-    //                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    //                 </p>
-    //             </div>
-    //         </foreignObject>
-    //     `
-    // });
-
-
-    // const card = new Card({
-    //     position: { x: 10, y: 10 },
-    //     attrs: {
-    //         body: { width: 180, height: 100 },
-    //         // foreignObject: { width: 168, height: 88 },
-    //     }
-    // });
-    
-    // card.addTo(graph);
-
-
+ 
     paper.unfreeze();
 }());
