f2ffa2ca0cc8d7d7f4640f94059fcc1f043c8a13
zbynekstara <zbynek.stara.internet@hotmail.com>
2023-08-22 15:14:39 +0200

    chore: monorepo architecture (part 1) [dev]

---
+++ packages/joint-decorators/src/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @module decorators
+ * @category joint
+ */
+
+export * from './decorators/Model';
+export * from './decorators/View';
+export * from './decorators/On';
+export * from './decorators/Function';
+export * from './decorators/SVGAttribute';
