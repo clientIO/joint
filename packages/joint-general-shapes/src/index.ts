f46567213449463930b7bd20fdec9be6fa586de9
Zbynek Stara <zbynek.stara.internet@hotmail.com>
2023-08-23 18:40:15 +0200

    chore: monorepo architecture (part 1) [dev] (#2309)

---
+++ packages/joint-general-shapes/src/index.ts
@@ -0,0 +1,28 @@
+/**
+ * @module general-shapes
+ * @category joint
+ */
+
+// import LinkedProcess from './linked-process';
+// import Input from './input';
+// import Mark from './mark';
+// import Actor from './actor';
+
+// export { LinkedProcess, Input, Mark, Actor };
+
+export * from './linked-process';
+export * from './input';
+export * from './mark';
+export * from './actor/actor';
+export * from './parallelogram/parallelogram';
+export * from './hexagon/hexagon';
+export * from './step/step';
+export * from './trapezoid/trapezoid';
+export * from './document/document';
+export * from './shipment/shipment';
+export * from './plus/plus';
+export * from './arrow/arrow';
+export * from './note/note';
+export * from './table/table';
+export * from './cube/cube';
+export * from './card/card';
