8ca05a7091609181e61d29e9c66c62ea1c8ebd04
Roman Bruckner <bruckner.roman@gmail.com>
2023-10-23 11:39:30 +0200

    remove lodash3 from qunit

--- grunt/config/qunit.js
+++ grunt/config/qunit.js
@@ -2,7 +2,6 @@ module.exports = {
     joint: [
         'test/jointjs/requirejs.html',
         'test/jointjs/browserify.html',
-        'test/jointjs/lodash3/index.html'
     ],
     geometry: [
         'test/geometry/requirejs.html',
