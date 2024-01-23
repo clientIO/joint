b39a47c27777788b9f3677705d66f6cb29995b57
Arthur Khokhlov <arthur.khokhlov@gmail.com>
2024-01-23 10:29:13 +0100

    fix

--- packages/joint-core/types/index.d.ts
+++ packages/joint-core/types/index.d.ts
@@ -1,3 +1,53 @@
-export * from './geometry';
-export * from './vectorizer';
-export * from './joint';
+import { g } from './geometry';
+import {
+    V,
+    Vectorizer,
+    VElement
+} from './vectorizer';
+import {
+    version,
+    config,
+    dia,
+    highlighters,
+    shapes,
+    util,
+    env,
+    layout,
+    mvc,
+    routers,
+    connectors,
+    anchors,
+    linkAnchors,
+    connectionPoints,
+    connectionStrategies,
+    attributes,
+    setTheme,
+    elementTools,
+    linkTools
+} from './joint';
+
+export {
+    g,
+    V,
+    Vectorizer,
+    VElement,
+    version,
+    config,
+    dia,
+    highlighters,
+    shapes,
+    util,
+    env,
+    layout,
+    mvc,
+    routers,
+    connectors,
+    anchors,
+    linkAnchors,
+    connectionPoints,
+    connectionStrategies,
+    attributes,
+    setTheme,
+    elementTools,
+    linkTools
+};
