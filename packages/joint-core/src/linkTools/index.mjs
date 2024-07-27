07f018d50d73d4d0f4c6b34e73d65202d9f20cc3
Roman Bruckner <bruckner.roman@gmail.com>
2024-07-27 21:38:31 +0200

    feat(linkTools): add Control and RotateLabel link tool

--- packages/joint-core/src/linkTools/index.mjs
+++ packages/joint-core/src/linkTools/index.mjs
@@ -1,8 +1,11 @@
 export * from './Vertices.mjs';
 export * from './Segments.mjs';
 export * from './Arrowhead.mjs';
-export * from './Boundary.mjs';
 export * from './Anchor.mjs';
-export * from './Button.mjs';
-export * from './Connect.mjs';
-export * from './HoverConnect.mjs';
+export * from './RotateLabel.mjs';
+
+export * from '../cellTools/Button.mjs';
+export * from '../cellTools/Boundary.mjs';
+export * from '../cellTools/Connect.mjs';
+export * from '../cellTools/HoverConnect.mjs';
+export * from '../cellTools/Control.mjs';
