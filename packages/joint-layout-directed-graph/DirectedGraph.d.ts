065b5a53e5f457a8333d6c2be513aab645e68e0d
zbynekstara <zbynek.stara.internet@hotmail.com>
2025-01-07 12:06:13 +0100

    add checkContainerConnections option to types

--- packages/joint-layout-directed-graph/DirectedGraph.d.ts
+++ packages/joint-layout-directed-graph/DirectedGraph.d.ts
@@ -38,6 +38,7 @@ export namespace DirectedGraph {
         rankSep?: number;
         marginX?: number;
         marginY?: number;
+        checkContainerConnections: boolean;
         resizeClusters?: boolean;
         clusterPadding?: dia.Padding | 'default';
         debugTiming?: boolean;
