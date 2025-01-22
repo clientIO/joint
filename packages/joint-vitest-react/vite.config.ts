3eb636d38b77f57be5abe4f8f0d6f1f7362c9b39
zbynekstara <zbynek.stara.internet@hotmail.com>
2025-01-22 14:32:01 +0100

    add SVGSVGElement prototype functions mock

--- packages/joint-vitest-react/vite.config.ts
+++ packages/joint-vitest-react/vite.config.ts
@@ -15,6 +15,6 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     //globals: true,
-    setupFiles: './src/setupTests.ts',
+    //setupFiles: './src/setupTests.ts',
   },
 });
