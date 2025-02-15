783bbbf03506c57c3ae97599b7f6f01509ff11a5
Martin Kaněra <marti.kanera@gmail.com>
2025-02-15 21:37:58 +0100

    feat(types): add protected getSnapPoint method to ElementView and LinkView

--- packages/joint-core/types/joint.d.ts
+++ packages/joint-core/types/joint.d.ts
@@ -1097,6 +1097,8 @@ export namespace dia {
         protected clearEmbedding(data: any): void;
 
         protected finalizeEmbedding(data: any): void;
+
+        protected getSnapPoint(): dia.Point;
     }
 
     // dia.LinkView
@@ -1262,6 +1264,8 @@ export namespace dia {
         protected mountLabels(): void;
 
         protected unmountLabels(): void;
+
+        protected getSnapPoint(): dia.Point;
     }
 
     // dia.Paper
