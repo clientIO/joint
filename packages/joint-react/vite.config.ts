bd6c8df4159a34f2caa91e540d835fcc231e7888
samuel.gjabel <samuel.gjabel@gmail.com>
2025-04-07 15:48:31 +0700

    feat(joint-react): eslint mixup

--- packages/joint-react/vite.config.ts
+++ packages/joint-react/vite.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable unicorn/prefer-module */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import mdPlugin from 'vite-plugin-md';
@@ -8,7 +9,7 @@ export default defineConfig({
   assetsInclude: ['**/*.md'],
   build: {
     lib: {
-      entry: path.resolve(import.meta.dirname, 'src/index.ts'),
+      entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'JointReact',
       fileName: (format) => `joint-react.${format}.js`,
     },
