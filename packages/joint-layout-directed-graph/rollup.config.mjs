183453c671fd21011886522e54ff7017670ef951
stanislav <sgribushin@gmail.com>
2025-04-14 13:57:32 +0200

    chore: update import assert (#2926)

--- packages/joint-layout-directed-graph/rollup.config.mjs
+++ packages/joint-layout-directed-graph/rollup.config.mjs
@@ -1,13 +1,11 @@
-import { readFileSync } from 'fs';
+import packageJson from './package.json' with { type: 'json' };
 import banner from 'rollup-plugin-banner2';
 import { uglify } from 'rollup-plugin-uglify';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-const pkg = JSON.parse(readFileSync('./package.json'));
-
 const today = new Date();
 const formattedDate = today.toLocaleDateString("en-US", { year: 'numeric' }) + "-" + today.toLocaleDateString("en-US", { month: '2-digit' }) + "-" + today.toLocaleDateString("en-US", { day: '2-digit' });
-const bannerText = `/*! ${pkg.title} v${pkg.version} (${formattedDate}) - ${pkg.description}\n\n\nThis Source Code Form is subject to the terms of the Mozilla Public\nLicense, v. 2.0. If a copy of the MPL was not distributed with this\nfile, You can obtain one at http://mozilla.org/MPL/2.0/.\n*/\n`;
+const bannerText = `/*! ${packageJson.title} v${packageJson.version} (${formattedDate}) - ${packageJson.description}\n\n\nThis Source Code Form is subject to the terms of the Mozilla Public\nLicense, v. 2.0. If a copy of the MPL was not distributed with this\nfile, You can obtain one at http://mozilla.org/MPL/2.0/.\n*/\n`;
 
 const input = ['./DirectedGraph.mjs'];
 
