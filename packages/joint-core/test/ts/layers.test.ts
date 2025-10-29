c5c95e84dcb396925485132cd75785529c8fa201
Roman Bruckner <bruckner.roman@gmail.com>
2025-10-29 21:26:37 +0100

    test

--- packages/joint-core/test/ts/layers.test.ts
+++ packages/joint-core/test/ts/layers.test.ts
@@ -8,5 +8,11 @@ graph.addLayer(layer1);
 graph.addLayer({ id: 'layer2', layerAttribute: 2 }, { flag: 1 });
 graph.moveLayer('layer1', { before: 'cells' });
 graph.moveLayer(layer1, { index: 2,flag: 1 });
+graph.hasLayer('cells');
+const layer2 = graph.getLayer('layer2');
+if (graph.hasLayer('layer2')) {
+  const layer2CellCount = layer2.cellCollection.length;
+}
+const layers = graph.getLayers();
 graph.removeLayer('layer2');
-graph.removeLayer(layer1, { flag: 1});
+graph.removeLayer(layer1, { flag: 1 });
