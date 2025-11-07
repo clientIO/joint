8918b5fb6d2d24e9cbbe3d3d7fb047ec69a0381d
zbynekstara <zbynek.stara.internet@hotmail.com>
2025-11-07 20:13:09 +0100

    adjust

--- examples/layout-elk-ts/src/index.ts
+++ examples/layout-elk-ts/src/index.ts
@@ -50,7 +50,7 @@ const init = () => {
         updateGraph(elkGraph as ElkGraph, graph);
         paper.unfreeze();
         zoom(paper, 1);
-        window.scroll(640, 560);
+        window.scroll(650, 560);
     }).catch((error) => {
         console.error('ELK layout error:', error.message);
     });
