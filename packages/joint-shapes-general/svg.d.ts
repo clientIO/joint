b7321df9db2122f08c06e5d7243adaf6ec481207
samuelgja <samuel.gjabel@gmail.com>
2026-01-10 11:32:52 +0700

    chore: update demo files to reference createPaper from the window object and clean up ESLint configurations

--- packages/joint-shapes-general/svg.d.ts
+++ packages/joint-shapes-general/svg.d.ts
@@ -1,5 +1,5 @@
 declare module '*.svg' {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const value: any;
-  export default value;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const value: any;
+    export default value;
 }
