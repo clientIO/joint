50a16f6b57510ba761ec8a3349676c8e1ff59c18
Samuel <samuel.gjabel@gmail.com>
2026-02-11 19:33:28 +0700

    chore(joint-react): remove snapshot tests (#3175)

--- packages/joint-react/src/components/highlighters/__tests__/custom.test.tsx
+++
@@ -1,9 +0,0 @@
-import { runStorybookSnapshot } from '../../../utils/run-storybook-snapshot';
-import { Custom } from '../custom';
-import * as stories from '../custom.stories';
-runStorybookSnapshot({
-  Component: Custom,
-  stories,
-  name: 'Highlighters/Custom',
-  withRenderElementWrapper: true,
-});
