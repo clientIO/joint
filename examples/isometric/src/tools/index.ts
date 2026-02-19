006ab2e9c512b0d7ab51489e82b9704cd8e643e4
Arthur Khokhlov <arthur.khokhlov@gmail.com>
2026-02-19 12:18:12 +0100

    removing demos and examples

--- examples/isometric/src/tools/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './center-based-height-tool';
-export * from './pyramid-height-tool';
-export * from './size-tool';
-export * from './proportional-size-tool';
-export * from './tools';
