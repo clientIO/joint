d2494a63ca8e96527ef5564f8efddea2985411be
Roman Bruckner <bruckner.roman@gmail.com>
2026-03-05 21:52:43 +0100

    update

--- packages/joint-react/src/stories/examples/with-embedding/code.tsx
+++ packages/joint-react/src/stories/examples/with-embedding/code.tsx
@@ -8,6 +8,7 @@ import {
   useElement,
   useElements,
   useGraph,
+  useGraphEvents,
   type FlatElementData,
   type RenderElement,
 } from '@joint/react';
@@ -103,13 +104,7 @@ function snapshot(graph: dia.Graph) {
 function useRawAttributes() {
   const graph = useGraph();
   const [attributes, setAttributes] = useState(() => snapshot(graph));
-  useEffect(() => {
-    const update = () => setAttributes(snapshot(graph));
-    graph.on('change', update);
-    return () => {
-      graph.off('change', update);
-    };
-  }, [graph]);
+  useGraphEvents(graph, { change: () => setAttributes(snapshot(graph)) });
   return attributes;
 }
 
