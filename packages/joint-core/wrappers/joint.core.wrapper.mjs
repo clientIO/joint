46a2f3aa19bfa7c1552cff7ee20431964b272622
zbynekstara <zbynek.stara.internet@hotmail.com>
2026-04-16 19:11:07 +0200

    simplify wrappers

--- packages/joint-core/wrappers/joint.core.wrapper.mjs
+++ packages/joint-core/wrappers/joint.core.wrapper.mjs
@@ -1,13 +1,6 @@
 import '../src/polyfills/index.mjs';
-import * as standard from '../src/shapes/standard.mjs';
 
-const shapes = { standard };
-const format = {};
-const ui = {};
-
+// joint core
 export * from '../src/core.mjs';
-export {
-    format,
-    ui,
-    shapes,
-};
+
+export * as shapes from '../src/shapes/index.mjs';
--- packages/joint-core/wrappers/joint.wrapper.mjs
+++ packages/joint-core/wrappers/joint.wrapper.mjs
@@ -1,9 +1,5 @@
 import '../src/polyfills/index.mjs';
 
-// export empty namespaces - backward compatibility
-export const format = {};
-export const ui = {};
-
 // joint core
 export * from '../src/core.mjs';
 
