e81090619003516029833817c6eb933509863892
zbynekstara <zbynek.stara.internet@hotmail.com>
2026-04-21 11:24:33 +0200

    typo

--- packages/joint-core/test/ts-exports/exports.test.ts
+++ packages/joint-core/test/ts-exports/exports.test.ts
@@ -1,6 +1,6 @@
 // Regression test for `types` path patterns in package.json `exports`.
 // - Compiled with `moduleResolution: "bundler"` to force checking `exports`.
 // - Relies on self-reference behavior which applies when `exports` are present.
-// - If either path pattern stops working, this file to fail to type-check.
+// - If either path pattern stops working, this file will fail to type-check.
 export type { Graph as DiaGraphExtensionless } from '@joint/core/types/dia';
 export type { Graph as DiaGraphExplicit } from '@joint/core/types/dia.d.ts';
