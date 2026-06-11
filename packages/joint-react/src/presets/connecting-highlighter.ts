998c0b0ef0adf2832bc705560c99b139b7cbdd44
Arthur Khokhlov <arthur.khokhlov@gmail.com>
2026-06-11 15:55:14 +0200

    class names normalization

--- packages/joint-react/src/presets/connecting-highlighter.ts
+++ packages/joint-react/src/presets/connecting-highlighter.ts
@@ -1,5 +1,5 @@
 import { dia, highlighters, V } from '@joint/core';
-import { SNAPPED_CLASS_NAME } from './link-view';
+import { CONNECTING_CLASS_NAME } from './link-view';
 
 /** Name used to register this highlighter in the paper's highlighterNamespace. */
 export const CONNECTING_HIGHLIGHTER_NAME = 'connecting';
@@ -10,16 +10,16 @@ export const CONNECTING_HIGHLIGHTER_NAME = 'connecting';
  * allows CSS @starting-style transitions to animate the highlight's appearance.
  */
 export const ConnectingHighlighter = highlighters.stroke.extend({
-    className: `jj-highlight-connecting`,
+    className: `jj-connecting-highlighter`,
     classNamePrefix: '',
 
     highlight(this: highlighters.stroke, cellView: dia.CellView, node: SVGElement): void {
-        V(node).addClass(SNAPPED_CLASS_NAME);
+        V(node).addClass(CONNECTING_CLASS_NAME);
         highlighters.stroke.prototype.highlight.call(this, cellView, node);
     },
 
     unhighlight(_cellView: dia.CellView, node: SVGElement): void {
-        V(node).removeClass(SNAPPED_CLASS_NAME);
+        V(node).removeClass(CONNECTING_CLASS_NAME);
         // @ts-expect-error - calling protected method of parent class
         highlighters.stroke.prototype.unhighlight.call(this, _cellView, node);
     },
