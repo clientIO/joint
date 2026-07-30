3923e62ab251d50b8e30c8af55c0c4b2fccec5e5
Arthur Khokhlov <arthur.khokhlov@gmail.com>
2026-07-30 12:32:49 +0200

    update

--- examples/avoid-router/src/index.js
+++ examples/avoid-router/src/index.js
@@ -31,4 +31,4 @@ document.querySelectorAll('.tab-button').forEach((button) => {
     button.addEventListener('click', () => activateTab(button.dataset.tab));
 });
 
-activateTab('simple');
+activateTab('large');
