f48152fecec8cfc07b53029f7844186a9b0fce3d
Arthur Khokhlov <arthur.khokhlov@gmail.com>
2026-08-04 14:09:53 +0200

    wip

---
+++ packages/joint-core/types/alg.d.ts
@@ -0,0 +1,20 @@
+import type * as dia from './dia.d.ts';
+import type * as g from './geometry.d.ts';
+
+export interface RightAnglePathOptions {
+    minPathMargin?: number;
+    targetInSourceBBox?: boolean;
+}
+
+export interface RightAnglePathEnd {
+    point: g.Point,
+    x0: number,
+    y0: number,
+    width: number,
+    height: number,
+    margin: number,
+    side: g.RectangleSide
+}
+
+
+export function rightAnglePath(source: RightAnglePathEnd, target: RightAnglePathEnd, options?: RightAnglePathOptions): dia.Point[];
